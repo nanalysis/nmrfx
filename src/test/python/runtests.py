cbbd85b962c9a68f2519781dcd15ddacccaf380a
Bruce Johnson <brucejohnson@OneMoonMBPro2.local>
2017-04-24 20:04:06 -0400

    Initial commit to repository

--- src/test/python/runtests.py
+++ src/test/python/runtests.py
@@ -5,11 +5,13 @@ import sys
 #all test files have to be loaded into the current namespace to be able to
 # execute them with unittest.main(), so we have to do: "from x import *"
 #jython script
-from teststruct import *
+#from testvec import *
+#from testoperations import *
+#from testpyproc import *
 
-if __name__ == "__main__":
-    unittest.main()
-    result = unittest.TestResult()
-    if result.errors:
-        sys.exit(1)
+#if __name__ == "__main__":
+#    unittest.main()
+#    result = unittest.TestResult()
+#    if result.errors:
+#        sys.exit(1)
         #raise java.lang.RuntimeException("%s errors" % result.errors)
