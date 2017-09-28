2ffdcda1ae4949619e5ecb41901890fc822ecc3b
Bruce Johnson <bruce.johnson@asrc.cuny.edu>
2017-09-28 18:06:29 -0400

    Add protein prediction code to predictor.  Choose rna/protein based on residues

--- src/main/resources/predictor.py
+++ src/main/resources/predictor.py
@@ -6,6 +6,8 @@ import runpy
 import rnapred
 from org.yaml.snakeyaml import Yaml
 from java.io import FileInputStream
+from org.nmrfx.structure.chemistry import ProteinPredictor
+
 
 
 homeDir =  os.getcwd( )
@@ -13,6 +15,59 @@ dataDir = homeDir + '/'
 outDir = os.path.join(homeDir,'output')
 
 argFile = sys.argv[-1]
+
+def predictProtein(mol, tableMode=False):
+    polymers = mol.getPolymers()
+    if tableMode:
+        for atomName in ('N','CA','CB','C','H','HA(2)','HA3'):
+            print atomName,
+        print ""
+    for polymer in polymers:
+        predictor = ProteinPredictor(polymer)
+        for residue in polymer.iterator():
+            if tableMode:
+                print residue.getNumber(), residue.getName(),
+                for atomName in ('N','CA','CB','C','H','HA','HA3'):
+                    if residue.getName() == "GLY" and atomName == 'HA':
+                        atomName = 'HA2'
+                    atom = residue.getAtom(atomName)
+                    if atom == None:
+                        print "  _   ",
+                    else:
+                        value = predictor.predict(residue.getAtom(atomName), False)
+                        if value != None:
+                            valueStr = "%6.2f" % (value)
+                            print valueStr,
+                        else:
+                            print "  _   ",
+                print ""
+            else:
+                for atomName in ('N','CA','CB','C','H','HA','HA3'):
+                    if residue.getName() == "GLY" and atomName == 'HA':
+                        atomName = 'HA2'
+                    atom = residue.getAtom(atomName)
+                    if atom != None:
+                        value = predictor.predict(residue.getAtom(atomName), False)
+                        if value != None:
+                            valueStr = "%s.%s %.2f" % (residue.getNumber(),atomName,value)
+                            print valueStr
+
+
+
+
+def isRNA(mol):
+    polymers = mol.getPolymers()
+    rna = False
+    for polymer in polymers:
+        for residue in polymer.getResidues():
+            resName = residue.getName()
+            if resName in ["A","C","G","U"]:
+                rna = True
+                break
+    return rna
+                
+
+
 
 if argFile.endswith('.yaml'):
     input = FileInputStream(argFile)
@@ -30,7 +85,11 @@ if argFile.endswith('.yaml'):
 elif argFile.endswith('.pdb'):
     refiner=refine()
     refiner.readPDBFile(argFile)
-    shifts = refiner.predictShifts()
-    for atomShift in shifts:
-        aname,shift = atomShift
-        print aname,shift
+    # fixme, need to do this by polymer so you can have protein-rna complex
+    if isRNA(refiner.molecule):
+        shifts = refiner.predictShifts()
+        for atomShift in shifts:
+            aname,shift = atomShift
+            print aname,shift
+    else:
+        predictProtein(refiner.molecule)
