db34ae771193a6a434dba9bb88296b99221a731c
Bruce Johnson <bruce.johnson@asrc.cuny.edu>
2017-12-27 09:38:15 -0500

    Add nwyaml (to read/write window state from yaml files)

---
+++ src/main/resources/nwyaml.py
@@ -0,0 +1,71 @@
+from org.yaml.snakeyaml import Yaml
+from java.io import FileInputStream
+import gscript
+
+def dumpYamlWin(yamlFile):
+    yaml=Yaml()
+    lims = nw.lim()
+    win={}
+    win['geometry'] = nw.geometry()
+    win['title'] = "Test"
+    win['grid'] = nw.getGrid()
+    (rows, cols) = win['grid']
+    spectra=[]
+    win['spectra'] = spectra
+    nCharts = nw.nCharts()
+    for iSpectrum in range(nCharts):
+        nw.active(iSpectrum)
+        sd = {}
+        spectra.append(sd)
+        sd['name'] = "duck"
+        iRow = iSpectrum / cols
+        iCol = iSpectrum % cols
+        sd['grid'] = [iRow, iCol]
+        sd['lim']=lims
+        sd['datasets'] = []
+        datasets = nw.datasets() 
+        for dataset in datasets:
+            dset = {}
+            dset['name']=dataset
+            dset['config']= nw.config(dataset)
+            sd['datasets'].append(dset)
+
+    print win
+    yamlDump = yaml.dump(win)
+    with open(yamlFile,'w') as fOut:
+        fOut.write(yamlDump)
+
+def loadYamlWin(yamlFile):
+    input = FileInputStream(yamlFile)
+    yaml = Yaml()
+    data = yaml.load(input)
+    input.close()
+    if 'geometry' in data:
+        (x,y,w,h) = data['geometry']
+        nw.geometry(x,y,w,h)
+    if 'grid' in data:
+        (rows,cols) = data['grid']
+        nw.grid(rows,cols)
+    spectra = data['spectra']
+    for v in spectra :
+        print v
+        datasets = v['datasets']
+        (iRow, iCol) = v['grid']
+        activeWin = iRow*cols+iCol
+        print 'g',iRow,iCol,activeWin
+        nw.active(activeWin)
+        lim = v['lim']
+        datasetValues = []
+        for dataset in datasets:
+            print dataset
+            name = dataset['name']
+            datasetValues.append(name)
+        print 'dv',datasetValues
+        nw.cmd.datasets(datasetValues)
+        nw.lim(lim)
+        for dataset in datasets:
+            name = dataset['name']
+            cfg = dataset['config']
+            nw.config(datasets=[name],pars=cfg)
+
+nw = gscript.NMRFxWindowScripting()
