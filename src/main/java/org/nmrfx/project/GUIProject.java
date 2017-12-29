f60357dc6f0064b876455bd8a91a2bc03af4b064
Bruce Johnson <bruce.johnson@asrc.cuny.edu>
2017-12-29 18:35:32 -0500

    Move GUIProject to a new org.nmrfx.project package

--- src/main/java/org/nmrfx/project/GUIProject.java
+++ src/main/java/org/nmrfx/project/GUIProject.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.nmrfx.processor.project;
+package org.nmrfx.project;
 
+import org.nmrfx.project.Project;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.FileSystem;
