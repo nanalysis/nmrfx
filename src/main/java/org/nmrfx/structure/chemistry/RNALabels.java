5ed4e03e9d1d5373c94e56646d7bf9f418b3e247
Bruce Johnson <bruce.johnson@asrc.cuny.edu>
2018-02-21 12:57:38 -0500

    Support new format for selection groups for labeling schemes

--- src/main/java/org/nmrfx/structure/chemistry/RNALabels.java
+++ src/main/java/org/nmrfx/structure/chemistry/RNALabels.java
@@ -31,13 +31,16 @@ public class RNALabels {
     static Pattern pattern2 = Pattern.compile("([0-9]+)-([0-9]+)");
     static Pattern pattern3 = Pattern.compile("([^:]+):?");
 
+    static Pattern pattern4 = Pattern.compile("(\\w+):([ACGU](,[ACGU])*)?([0-9]+)?-?([0-9]+)?\\.([a-zA-Z]+\\w*)");
+    static Pattern pattern = Pattern.compile("(\\*|\\w+):([ACGU](,[ACGU])*)?((([0-9]+)?-?([0-9]+)?)|\\*)\\.(([HCN][^, ]*)(,[HCN][^, ]*)*)$");
+
     public void parse(Molecule molecule, String selGroup) {
         String[] groups = selGroup.split(" ");
         if (groups.length < 2) {
             throw new IllegalArgumentException("Improper selGroup format " + selGroup);
         }
         String entityRes = groups[0];
-        Matcher matcher = pattern1.matcher(entityRes);
+        Matcher matcher = pattern.matcher(entityRes);
         Integer startRes = null;
         Integer endRes = null;
         String eS = "";
@@ -139,4 +142,157 @@ public class RNALabels {
             }
         }
     }
+
+    public void parseSelGroups(Molecule molecule, String selGroups) {
+        List<Atom> allAtoms = molecule.getAtomArray();
+        allAtoms.forEach((atom) -> {
+            atom.setActive(false);
+        });
+        String[] groups = selGroups.split(" ");
+        for (String group : groups) {
+            group = group.trim();
+            if (group.length() > 0) {
+                parseGroup(molecule, group);
+            }
+        }
+    }
+
+    public void parseGroup(Molecule molecule, String group) {
+        SelGroup selGroup = parseSelGroup(group);
+        List<Polymer> polymers = molecule.getPolymers();
+        String entityStr = selGroup.entityStr;
+        for (Polymer polymer : polymers) {
+            if (entityStr.equals("*") || entityStr.equals(polymer.getName())) {
+                List<Residue> residues = polymer.getResidues();
+                for (Residue residue : residues) {
+                    String resName = residue.getName();
+                    String resNumStr = residue.getNumber();
+
+                    int resNum = Integer.parseInt(resNumStr);
+                    boolean resMatches = checkResType(resName, selGroup.resTypes);
+
+                    if (resMatches) {
+                        if ((selGroup.firstRes != null) && (resNum < selGroup.firstRes)) {
+                            continue;
+                        }
+                        if ((selGroup.lastRes != null) && (resNum > selGroup.lastRes)) {
+                            continue;
+                        }
+
+                        List<Atom> atoms = residue.getAtoms();
+                        for (Atom atom : atoms) {
+                            boolean ribose = false;
+                            boolean exchangable = false;
+                            if ((atom.parent != null) && (atom.getParent().getElementName().equals("O"))) {
+                                continue;
+                            }
+                            if (atom.getName().endsWith("'") || atom.getName().endsWith("\"")) {
+                                ribose = true;
+                            } else if (atom.getElementName().equals("H") && atom.getParent().getElementName().equals("N")) {
+                                exchangable = true;
+                            }
+                            boolean ok = checkAtom(atom.getName(), atom.getElementName(), selGroup.gAtomNames, ribose, exchangable);
+                            if (ok) {
+                                atom.setActive(true);
+                            }
+                        }
+                    }
+
+                }
+
+            }
+        }
+    }
+
+    public static class SelGroup {
+
+        public final Integer firstRes;
+        public final Integer lastRes;
+        public final String entityStr;
+        public final String[] resTypes;
+        public final String[] gAtomNames;
+
+        SelGroup(Integer startRes, Integer endRes, String entityStr, String[] resTypes, String[] gAtomNames) {
+            this.firstRes = startRes;
+            this.lastRes = endRes;
+            this.entityStr = entityStr;
+            this.resTypes = resTypes;
+            this.gAtomNames = gAtomNames;
+        }
+    }
+
+    public static SelGroup parseSelGroup(String group) {
+        Matcher matcher = pattern.matcher(group.trim());
+        Integer startRes = null;
+        Integer endRes = null;
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("Group " + group + " can't be parsed");
+        }
+        String entityStr = matcher.group(1);//2,4,,5,6
+        String resTypeStr = matcher.group(2);
+        String resRangeStr = matcher.group(4);
+        String startResStr = matcher.group(6);
+        String endResStr = matcher.group(7);
+        if ((startResStr != null) && (startResStr.length() > 0)) {
+            startRes = Integer.parseInt(startResStr);
+        }
+        if ((endResStr != null) && (endResStr.length() > 0)) {
+            endRes = Integer.parseInt(endResStr);
+        }
+        String atomStr = matcher.group(8);
+        String[] resTypes = new String[0];
+        if (resTypeStr != null) {
+            resTypes = resTypeStr.split(",");
+        }
+        String[] gAtomNames = atomStr.split(",");
+        if (entityStr.equals("")) {
+            entityStr = "*";
+        }
+
+        SelGroup selGroup = new SelGroup(startRes, endRes, entityStr, resTypes, gAtomNames);
+        return selGroup;
+
+    }
+
+    public static boolean checkResType(String resName, String[] resTypes) {
+        boolean resMatches = resTypes.length == 0;
+        for (String resType : resTypes) {
+            if (resType.equals(resName)) {
+                resMatches = true;
+                break;
+            }
+        }
+        return resMatches;
+    }
+
+    public static boolean checkAtom(String aName, String atomElement, String[] gAtomNames, boolean ribose, boolean exchangable) {
+        boolean ok = false;
+        for (String gAtomName : gAtomNames) {
+            if (ribose && atomElement.equals("H") && gAtomName.equals("Hr")) {
+                ok = true;
+                break;
+            } else if (ribose && atomElement.equals("C") && gAtomName.equals("Cr")) {
+                ok = true;
+                break;
+            } else if (!ribose && !exchangable && atomElement.equals("H") && gAtomName.equals("Hn")) {
+                ok = true;
+                break;
+            } else if (!ribose && !exchangable && atomElement.equals("C") && gAtomName.equals("Cn")) {
+                ok = true;
+                break;
+            } else if (exchangable && atomElement.equals("H") && gAtomName.equals("He")) {
+                ok = true;
+                break;
+            } else if (exchangable && atomElement.equals("N") && gAtomName.equals("Ne")) {
+                ok = true;
+                break;
+            } else if (gAtomName.equals(aName)) {
+                ok = true;
+                break;
+            }
+        }
+        return ok;
+
+    }
+
 }
