9995cd686fae803d26e439c4598cd7a9050bee93
Bruce Johnson <bruce.johnson@asrc.cuny.edu>
2018-04-22 15:31:15 -0400

    Add new prediction models (using more shifts and more atoms).  Set error Almost all rna C,H,N atoms now supported. Estimated error value now set on atom ref ppm

--- src/main/resources/rnapred.py
+++ src/main/resources/rnapred.py
@@ -23,6 +23,8 @@ from java.io import FileWriter
 from subprocess import check_output
 
 rnaDataValues = {}
+wc = {'G': 'C', 'C': 'G', 'A': 'U', 'U': 'A', 'X': '', 'x': '', 'P':'p','p':'P'}
+wobble = {'G': 'U', 'U': 'G', 'A': '', 'C': '', 'X': '', 'x': '','P':'p','p':'P'}
 
 def getPairs( vienna):
     sArray = [len(vienna)]
@@ -32,6 +34,19 @@ def getPairs( vienna):
     pairs = ssLay.getBasePairs()
     return pairs
 
+def checkNC(pairs,index):
+    nNucs = len(pairs)
+    type = '-'
+    if (index > 0) and (index < (nNucs-1)):
+        bp0 = pairs[index-1]
+        bp1 = pairs[index]
+        bp2 = pairs[index+1]
+        if abs(bp0-bp1) != 1:
+            type = '5prime-nc'
+        if abs(bp1-bp2) != 1:
+            type = '3prime-nc'
+        print "check",index,nNucs,bp0,bp1,bp2
+    return type
 
 def genRNAData(seqList, pairs):
     pairInfo = []
@@ -45,8 +60,6 @@ def genRNAData(seqList, pairs):
     gnraPat = re.compile('G-[AGUC]-[AG]-A-')
     uncgPat = re.compile('U-[AGUC]-C-G-')
 
-    wc = {'G': 'C', 'C': 'G', 'A': 'U', 'U': 'A', 'X': '', 'x': ''}
-    wobble = {'G': 'U', 'U': 'G', 'A': '', 'C': '', 'X': '', 'x': ''}
 
     matchPats = {'GC': 'Pp', 'GU': 'Pp', 'GA': 'PP', 'GG': 'PP', 'G-': 'P-', 'AC': 'Pm', 'AU': 'Pp', 'AA': 'PP',
                  'AG': 'PP', 'A-': 'P-', 'CC': 'pp', 'CU': 'pp', 'CA': 'pm', 'CG': 'pP', 'C-': 'p-', 'UC': 'pp',
@@ -75,7 +88,7 @@ def genRNAData(seqList, pairs):
             lastCoordEntity = coordEntity
 
         strType = "wc"
-        influ = "-"
+        influ = checkNC(pairs,index)
         attr = "-"
         type = end
         endNuc = ''
@@ -143,21 +156,94 @@ def genRNAData(seqList, pairs):
             kRes = iRes + jRes + 2
             attr = rnaData['attr'][kRes]
             outLine += attr + ","
+        influ = rnaData['influ'][iRes + 2]
+        outLine += influ + ","
         outLine = outLine[0:-1]
         outLines.append(outLine)
     return outLines
+
+def getType(attrs):
+    canonical=True
+    helix=True
+    tetraloop = False
+    isMisMatch=False
+    nucList = attrs[0:5]
+    attrList = attrs[5:10]
+    for attrVal in attrList[1:4]:
+        #  check this fixme
+        if attrVal != "-":
+            helix = False
+   
+    for attrVal in attrList[0:5]:
+        #  check this fixme
+        if attrVal != "-":
+            canonical = False
+   
+    for nucVal in nucList[0:5]:
+        if not checkWC(nucVal, True):
+            canonical = False
+
+    for nucVal in nucList[1:4]:
+        if not checkWC(nucVal, True):
+            helix = False
+   
+    for attrVal in attrList[1:4]:
+        if attrVal.startswith("gnra"):
+            tetraloop=True
+        elif attrVal.startswith("tetr"):
+            tetraloop=True
+
+    return (helix, canonical, tetraloop)
+
+def checkWC(nuc,strict):
+    matched = True
+    isWC = False
+    if nuc == "-":
+        matched = False
+    else:
+        n1,n2 = nuc
+        if n1 == "-":
+            matched = False
+        if n2 == "-":
+            matched = False
+        if matched:
+            if n2 == wc[n1]:
+                isWC = True
+            elif not strict:
+                if n2 == wobble[n1]:
+                    isWC = True
+    return isWC
+
+def getError(atom,helix,canonical,tetraloop):
+    global svmSDevMap
+    mode="c"
+    if not helix:
+       if tetraloop:
+           mode="nc"
+       else:
+           mode="o"
+    else:
+       if not canonical:
+           mode="nc"
+
+    errorVal= svmSDevMap[atom,mode]
+    return errorVal
 
 
 def predictFromAttr(seqList, outLines):
     global rnaDataValues
     atoms = (
-        'AH2', 'AH8', 'GH8', 'CH5', 'UH5', 'CH6', 'UH6', 'AH1p', 'GH1p', 'CH1p', 'UH1p', 'AH2p', 'GH2p', 'CH2p', 'UH2p',
-        'AH3p', 'GH3p', 'CH3p', 'UH3p', 'AC2', 'AC8', 'GC8', 'CC5', 'UC5', 'CC6', 'UC6', 'AC1p', 'GC1p', 'CC1p', 'UC1p',
-        'AC2p', 'GC2p', 'CC2p', 'UC2p', 'AC3p', 'GC3p', 'CC3p', 'UC3p')
-    types = ('nuc1', 'nuc2', 'nuc3', 'nuc4', 'nuc5', 'pos1', 'pos2', 'pos3', 'pos4', 'pos5')
+        'AC2','AC8','GC8','CC5','UC5','CC6','UC6','AC1p','GC1p','CC1p','UC1p','AC2p','GC2p','CC2p',
+        'UC2p','AC3p','GC3p','CC3p','UC3p','AH8','GH8','AH2','CH5','UH5','CH6','UH6','AH1p','GH1p',
+        'CH1p','UH1p','AH2p','GH2p','CH2p','UH2p','AH3p','GH3p','CH3p','UH3p','GH1','UH3','UH4p',
+        'UH5p','UH5pp','CH4p','CH41','CH42','CH5p','CH5pp','AH4p','AH5p','AH5pp','AH61','AH62',
+        'GH21','GH22','GH4p','GH5p','GH5pp','AN1','AN3','AN6','AN7','AN9','CN1','CN3','CN4','GN1',
+        'GN2','GN7','GN9','UN1','UN3')
+    types = ('nuc1', 'nuc2', 'nuc3', 'nuc4', 'nuc5', 'pos1', 'pos2', 'pos3', 'pos4', 'pos5','nuc')
     result = {}
     for (outLine, res) in zip(outLines, seqList):
         values = outLine.split(',')
+        (helix,canonical,tetraloop) = getType(values)
         attrValues = '_'.join(values)
         attributes = {}
         for type, value in zip(types, values):
@@ -169,7 +255,8 @@ def predictFromAttr(seqList, outLines):
             nuc = nucValues[2][0]
             if nuc == targetNuc:
                 shift = svmPredict(atom, attributes)
-                #print attributes,shift,res,atom
+                errorVal = getError(atom,helix,canonical,tetraloop)
+                #print attributes,res, atom, shift,errorVal
                 atomAttr = atom + '_' + attrValues
                 if atomAttr in rnaDataValues:
                     sValues = rnaDataValues[atomAttr].split()
@@ -184,7 +271,7 @@ def predictFromAttr(seqList, outLines):
                     wshift = f * shift + (1.0 - f) * mean
                     shift = wshift
                     #print res,targetNuc,targetAtom,attrValues,shift
-                result[res + '.' + targetAtom] = shift
+                result[res + '.' + targetAtom] = (shift, errorVal)
     return result
 
 
@@ -216,8 +303,10 @@ def loadResource(resourceName):
 def setupRNASVM():
     global svm
     global svmAttrMap
+    global svmSDevMap
     svm = SVMPredict()
     svmAttrMap = {}
+    svmSDevMap = {}
     resourceName = "data/rnasvm/svattr.txt"
     content = loadResource(resourceName)
     lines = content.split('\n')
@@ -230,6 +319,15 @@ def setupRNASVM():
         if not (atomName, 'attrs') in svmAttrMap:
             svmAttrMap[atomName, 'attrs'] = []
         svmAttrMap[atomName, 'attrs'].append(attrType)
+    resourceName = "data/rnapredsdev.txt"
+    content = loadResource(resourceName)
+    lines = content.split('\n')
+    for line in lines:
+        fields = line.split()
+        atomName = fields[0]
+        type = fields[1]
+        sdev = float(fields[2])
+        svmSDevMap[atomName,type] = sdev
 
 def loadRNAShifts():
     global rnaDataValues
@@ -281,12 +379,15 @@ def getFullSequence(molecule):
 def setPredictions(molecule, predPPMs, refMode=True):
     molecule.updateAtomArray()
     for atomName in predPPMs:
-        ppm = predPPMs[atomName]
+        ppm,errorVal = predPPMs[atomName]
         if atomName[-1] == 'p':
             atomName = atomName[0:-1] + "'"
+            if atomName[-2] == 'p':
+                atomName = atomName[0:-2] + "''"
         atom = Molecule.getAtomByName(atomName)
         if refMode:
             atom.setRefPPM(ppm)
+            atom.setRefError(errorVal)
         else:
             atom.setPPM(ppm)
 
