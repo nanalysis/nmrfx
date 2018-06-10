676cd03a1429183b259b23c1dde4e4d4233a668b
Bruce Johnson <brucejohnson@localhost>
2018-06-10 09:50:34 -0400

    Rename analyst startup scripts to be like nmrfxa

---
+++ src/main/scripts/nmrfxa.bat
@@ -0,0 +1,36 @@
+@echo off
+
+rem nvjp [ script  [ arg ... ] ]
+rem 
+rem optional environment variables:
+rem
+rem JAVA_HOME  - directory of JDK/JRE, if not set then 'java' must be found on PATH
+rem CLASSPATH  - colon separated list of additional jar files & class directories
+rem JAVA_OPTS  - list of JVM options, e.g. "-Xmx256m -Dfoo=bar"
+rem
+
+
+if "%OS%" == "Windows_NT" setlocal
+
+set nvjver=${project.version}
+set nvjpmain=org.python.util.jython
+
+set dir=%~dp0
+
+set javaexe=java
+set cp="%dir%\analsyt-%nvjver%.jar;${wclasspath};%CLASSPATH%"
+
+set testjava=%dir%jre\bin\java
+
+if exist %testjava% (
+    set javaexe="%testjava%"
+    set cp="%dir%\lib\analsyt-%nvjver%.jar;${wclasspath};%CLASSPATH%"
+)
+
+
+if "%1"=="" (
+    %javaexe% -Djava.awt.headless=true -mx2048m -cp %cp% %JAVA_OPTS% %nvjpmain%
+) else (
+    %javaexe% -Djava.awt.headless=true -mx2048m -cp %cp% %JAVA_OPTS% %nvjpmain% -c "import dispatchnvfx" %*
+)
+
