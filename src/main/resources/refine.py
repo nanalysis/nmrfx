06a7404fd06ec7d05c5aad18a804df4619671ee5
Bruce Johnson <bruce.johnson@asrc.cuny.edu>
2018-07-12 14:21:40 -0400

    Turn on irp force for all of anneal (was on just at beginning)

--- src/main/resources/refine.py
+++ src/main/resources/refine.py
@@ -1407,7 +1407,7 @@ class refine:
         rDyn.continueDynamics2(tempLambda,econLambda,stepsAnneal2,timeStep)
         rDyn.run(switchFrac)
 
-        self.setForces(repel=1.0, irp=-1.0, bondWt = 25.0)
+        self.setForces(repel=1.0, bondWt = 25.0)
         self.setPars(useh=True,hardSphere=0.0,shrinkValue=0.0,shrinkHValue=0.0)
         self.setPars(optDict=stage2)
 
