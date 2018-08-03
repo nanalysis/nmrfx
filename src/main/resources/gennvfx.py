cc02d1cca444105df9798198e20c9bf27ff60ef5
Bruce Johnson <bruce.johnson@asrc.cuny.edu>
2018-08-03 14:21:51 -0400

    Allow running a script during a gen subcommand. Script can be specified in .yaml file, or as an argument on command line after .yaml file.

--- src/main/resources/gennvfx.py
+++ src/main/resources/gennvfx.py
@@ -14,7 +14,6 @@ def parseArgs():
     parser.add_option("-d", "--directory", dest="directory",default=homeDir, help="Base directory for output files ")
 
     (options, args) = parser.parse_args()
-    print 'args',args
     homeDir = options.directory
     outDir = os.path.join(homeDir,'output')
     finDir = os.path.join(homeDir,'final')
@@ -33,15 +32,19 @@ def parseArgs():
         input = FileInputStream(argFile)
         yaml = Yaml()
         data = yaml.load(input)
-
+        global refiner
         refiner=refine()
         osfiles.setOutFiles(refiner,dataDir, seed)
         refiner.rootName = "temp"
         refiner.loadFromYaml(data,seed)
         if 'anneal' in data:
             refiner.anneal(refiner.dOpt)
+        if 'script' in data:
+            runpy.run_path(data['script'], init_globals=globals())
+        if len(args) > 1:
+            scriptFile = args[1]
+            runpy.run_path(scriptFile, init_globals=globals())
         refiner.output()
-
     else:
         runpy.run_path(argFile)
 
