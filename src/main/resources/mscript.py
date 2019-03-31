cd9725f1d1fbe9fb2cd91af7ee7bdd7d1f324a37
Bruce Johnson <bruce.johnson@asrc.cuny.edu>
2019-03-31 16:12:18 -0400

    Add mscript.py.  Useful for importing chemistry java classes to python

---
+++ src/main/resources/mscript.py
@@ -0,0 +1,5 @@
+from org.nmrfx.structure.chemistry import Molecule
+from org.nmrfx.structure.chemistry import Residue
+from org.nmrfx.structure.chemistry import Compound
+from org.nmrfx.structure.chemistry import Atom
+from org.nmrfx.structure.chemistry import Polymer
