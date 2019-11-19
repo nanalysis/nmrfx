9060f4ee67954ccf8a28497f0f3d905a085a50f9
bajlabuser <bajlabuser@johnsb33533.asrc.cuny.edu>
2019-11-19 14:18:52 -0500

    -Secondary Structure code refactor -Modified basepair interaction recognition -RNA basepair tolerance optimized slightly

--- src/main/java/org/nmrfx/structure/chemistry/Bulge.java
+++ src/main/java/org/nmrfx/structure/chemistry/Bulge.java
@@ -1,7 +1,19 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * NMRFx Structure : A Program for Calculating Structures 
+ * Copyright (C) 2004-2017 One Moon Scientific, Inc., Westfield, N.J., USA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.nmrfx.structure.chemistry;
 
@@ -13,15 +25,13 @@ import java.util.*;
  */
 public class Bulge extends SecondaryStructure {
 
-    public static int localind = 0;
+    public static int localCounter = 0;
+    public static String name = "Bulge";
 
-    public Bulge(String id, List<Residue> Bresidues) {
-        name = id;
-        locali = localind;
-        globali = globalind;
-        secresidues = Bresidues;
-        globalind++;
-        localind++;
+    public Bulge(List<Residue> residues) {
+        localIndex = localCounter++;
+        globalIndex = globalCounter++;
+        secResidues = residues;
     }
 
 }
