b2f65f1f0b84eba313a37c8fa867513a7b1f9f41
Bruce Johnson <brucejohnson@localhost>
2020-03-23 13:31:39 -0400

    Start adding compound fitter

--- src/main/java/org/nmrfx/processor/gui/SimMolController.java
+++ src/main/java/org/nmrfx/processor/gui/SimMolController.java
@@ -24,10 +24,13 @@ import javafx.scene.paint.Color;
 import javafx.util.Callback;
 import org.controlsfx.control.textfield.AutoCompletionBinding;
 import org.controlsfx.control.textfield.TextFields;
+import org.nmrfx.processor.compoundLib.CompoundMatcher;
+import org.nmrfx.processor.compoundLib.CompoundData;
 import org.nmrfx.processor.dataops.SimData;
 import org.nmrfx.processor.datasets.Dataset;
 import org.nmrfx.processor.gui.spectra.DatasetAttributes;
 import org.nmrfx.processor.gui.utils.ColorSchemes;
+import org.nmrfx.processor.math.Vec;
 
 public class SimMolController implements ControllerTool {
 
@@ -44,6 +47,7 @@ public class SimMolController implements ControllerTool {
     TextField maxField;
     Background defaultBackground = null;
     Background errorBackground = new Background(new BackgroundFill(Color.YELLOW, null, null));
+    CompoundMatcher cmpdMatcher = new CompoundMatcher();
 
     public SimMolController(FXMLController controller, Consumer<SimMolController> closeAction) {
         this.controller = controller;
@@ -92,12 +96,20 @@ public class SimMolController implements ControllerTool {
         molNameField.setPrefWidth(300);
         molNameField.setOnKeyReleased(e -> {
             if (e.getCode() == KeyCode.ENTER) {
-                setMol();
+                // setMol();
             }
         });
         toolBar.getItems().add(atomFieldLabel);
         toolBar.getItems().add(molNameField);
         addFiller(toolBar);
+        Button addButton = new Button("Show");
+        addButton.setOnAction(e -> setMol());
+        toolBar.getItems().add(addButton);
+
+        Button stdButton = new Button("Std");
+        stdButton.setOnAction(e -> createCmpdData());
+        toolBar.getItems().add(stdButton);
+
         Callback<AutoCompletionBinding.ISuggestionRequest, Collection<String>> suggestionProvider = new Callback<AutoCompletionBinding.ISuggestionRequest, Collection<String>>() {
             @Override
             public Collection<String> call(AutoCompletionBinding.ISuggestionRequest param) {
@@ -151,22 +163,98 @@ public class SimMolController implements ControllerTool {
             controller.getStatusBar().setMode(1);
             chart.setDataset(newDataset, true);
 
-            if (chart.getDatasetAttributes().size() > 1) {
-                chart.chartProps.setTitles(true);
-                int nData = chart.getDatasetAttributes().size() - 1;
-                String colScheme = (nData <= 10) ? "category10" : "category20";
-                List<Color> colors = ColorSchemes.getColors(colScheme, nData);
-                int i = 0;
-                for (DatasetAttributes dataAttr : chart.getActiveDatasetAttributes()) {
-                    if (i > 0) {
-                        dataAttr.setPosColor(colors.get(i - 1));
-                    }
-                    i++;
-                }
-            }
-            //chart.updateDatasets(names);
+            updateColors(chart);
             molNameField.setText("");
             chart.refresh();
+        }
+    }
+
+    public void createCmpdData() {
+        String name = molNameField.getText().toLowerCase();
+        String[] names = {"sum", "current"};
+        Dataset[] datasets = new Dataset[names.length];
+        System.out.println("Mol Name: " + name);
+        if (SimData.contains(name)) {
+            PolyChart chart = controller.getActiveChart();
+            for (int iDataset = 0; iDataset < datasets.length; iDataset++) {
+                datasets[iDataset] = Dataset.getDataset(names[iDataset]);
+            }
+
+            Dataset currData = null;
+            for (PolyChart pChart : controller.getCharts()) {
+                currData = pChart.getDataset();
+                if (currData != null) {
+                    break;
+                }
+            }
+            String label = currData != null ? currData.getLabel(0) : "1H";
+            double sf = currData != null
+                    ? currData.getSf(0) : AnalystPrefs.getLibraryVectorSF();
+            double lb = AnalystPrefs.getLibraryVectorLB();
+            double sw = currData != null
+                    ? currData.getSw(0) : AnalystPrefs.getLibraryVectorSW();
+            int size = currData != null
+                    ? currData.getSize(0)
+                    : (int) Math.pow(2, AnalystPrefs.getLibraryVectorSize());
+            double ref = currData != null
+                    ? currData.pointToPPM(0, size / 2) : AnalystPrefs.getLibraryVectorREF();
+
+            /*
+                public static CompoundData genCompoundData(String cmpdID, String name, int n,
+            double refConc, double cmpdConc,
+            double sf, double sw, double centerPPM, double lb, double frac) {
+
+             */
+            double refConc = 1.0;
+            double cmpdConc = 1.0;
+            double frac = 1.0e-3;
+            CompoundData cData = SimData.genCompoundData(name, name, size, refConc, cmpdConc, sf, sw, ref, lb, frac);
+            CompoundData.put(cData, name);
+            cmpdMatcher.addMatch(cData);
+            for (int iDataset = 0; iDataset < datasets.length; iDataset++) {
+                if (datasets[iDataset] == null) {
+                    Vec vec = SimData.prepareVec(names[iDataset], size, sf, sw, ref, lb);
+                    vec.setFreqDomain(true);
+                    double vRef = sw / sf / 2 + ref;
+                    vec.setRef(vRef);
+                    datasets[iDataset] = new Dataset(vec);
+                    datasets[iDataset].setLabel(0, label);
+                }
+            }
+            Vec sumVec = datasets[0].getVec();
+            cmpdMatcher.updateVec(sumVec);
+            Vec currentVec = datasets[1].getVec();
+            currentVec.zeros();
+            cData.addToVec(currentVec, 1.0);
+            if (currData != null) {
+                Vec expVec = currData.getVec();
+            }
+
+            controller.getStatusBar().setMode(1);
+            for (Dataset dataset : datasets) {
+                if (!chart.containsDataset(dataset)) {
+                    chart.setDataset(dataset, true);
+                }
+            }
+            updateColors(chart);
+            molNameField.setText("");
+            chart.refresh();
+        }
+    }
+
+    void updateColors(PolyChart chart) {
+        if (chart.getDatasetAttributes().size() > 1) {
+            chart.chartProps.setTitles(true);
+            int nData = chart.getDatasetAttributes().size() - 1;
+            String colScheme = (nData <= 10) ? "category10" : "category20";
+            List<Color> colors = ColorSchemes.getColors(colScheme, nData);
+            int i = 0;
+            for (DatasetAttributes dataAttr : chart.getActiveDatasetAttributes()) {
+                if (i > 0) {
+                    dataAttr.setPosColor(colors.get(i - 1));
+                }
+                i++;
+            }
         }
 
     }
