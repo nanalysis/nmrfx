63fbc11e276eae24cde7dbc4e64d5f4363cc9190
Bruce Johnson <bruce.johnson@asrc.cuny.edu>
2020-06-01 15:30:05 -0400

    Get NEF read/write working

--- src/test/java/org/nmrfx/structure/chemistry/NEFFileTest.java
+++ src/test/java/org/nmrfx/structure/chemistry/NEFFileTest.java
@@ -5,8 +5,17 @@
  */
 package org.nmrfx.structure.chemistry;
 
+import java.io.BufferedReader;
 import java.io.File;
-import org.apache.logging.log4j.core.util.FileUtils;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import org.junit.Assert;
 import org.junit.Test;
 import org.nmrfx.structure.chemistry.io.NMRNEFReader;
@@ -18,32 +27,363 @@ import org.nmrfx.structure.chemistry.io.NMRNEFWriter;
  */
 public class NEFFileTest {
 
-    String classPath = this.getClass().getResource("NEFFileTest.class").getPath();
-    String program = "nmrfxstructure";
-    String mainPath = classPath.substring(0, classPath.indexOf(program) + program.length());
-    String fileName = String.join(File.separator, mainPath, "sandbox", "1pqx.nef");
-    String outFile = String.join(File.separator, mainPath, "sandbox", "1pqx_nef_outTest.txt");
+    // ok 1pqx 2k2e 2kpu 2kw5 2loy 2luz
+    // HH% twice 2jr2 2juw 2kko
+    // HB% twice etc 2ko1
+    // Asp HD2 2kzn   should have residue modifier +HD2
+    // ILE CGx CGy  (these are not stereo equiv  2png
+    // has ligand 6nbn
+    List<List<Object>> orig = new ArrayList<>();
+    List<List<Object>> written = new ArrayList<>();
 
-//    @Test
-//    public void readNEF() {  
-//        try {
-//            NMRNEFReader.read(fileName);
-//        } catch (Exception ex) {
-//            ex.printStackTrace();
-//        }
-//        
-//    }
     @Test
-    public void writeNEF() {
+    public void testFile2KO1() throws IOException {
+        loadData("2ko1");
+        testAll();
+    }
+
+    @Test
+    public void testFile2PNG() throws IOException {
+        loadData("2png");
+        testAll();
+    }
+
+    @Test
+    public void testFile2KZN() throws IOException {
+        loadData("2kzn");
+        testAll();
+    }
+
+    @Test
+    public void testFile2KKO() throws IOException {
+        loadData("2kko");
+        testAll();
+    }
+
+    @Test
+    public void testFile2JUW() throws IOException {
+        loadData("2juw");
+        testAll();
+    }
+
+    @Test
+    public void testFile2JR2() throws IOException {
+        loadData("2jr2");
+        testAll();
+    }
+
+    @Test
+    public void testFile1PQX() throws IOException {
+        loadData("1pqx");
+        testAll();
+    }
+
+    @Test
+    public void testFile2K2E() throws IOException {
+        loadData("2k2e");
+        testAll();
+    }
+
+    @Test
+    public void testFile2KPU() throws IOException {
+        loadData("2kpu");
+        testAll();
+    }
+
+    @Test
+    public void testFile2KW5() throws IOException {
+        loadData("2kw5");
+        testAll();
+    }
+
+    @Test
+    public void testFile2LOY() throws IOException {
+        loadData("2loy");
+        testAll();
+    }
+
+    @Test
+    public void testFile2LUZ() throws IOException {
+        loadData("2luz");
+        testAll();
+    }
+
+    private List<List<Object>> convertFileLines(String filePath) throws FileNotFoundException, IOException {
+        List<List<Object>> convertedLines = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        while (true) {
+            String line = reader.readLine();
+            if (line == null) {
+                break;
+            }
+            List<String> sLine = Arrays.asList(line.trim().split("\\s+"));
+            List<Object> cLine = new ArrayList<>();
+            for (String s : sLine) {
+                try {
+                    cLine.add(Integer.parseInt(s));
+                } catch (NumberFormatException ex1) {
+                    try {
+                        cLine.add(Double.parseDouble(s));
+                    } catch (NumberFormatException ex2) {
+                        cLine.add(s);
+                    }
+                }
+            }
+            convertedLines.add(cLine);
+        }
+        return convertedLines;
+    }
+
+    private Map<String, List<Object>> buildSequenceMap(List<List<Object>> dataArray) {
+        Map<String, List<Object>> seqMap = new HashMap<>();
+        boolean inSeq = false;
+        for (List<Object> line : dataArray) {
+            if (line.size() > 0) {
+                if (line.get(0).toString().contains("_sequence.index")) {
+                    inSeq = true;
+                }
+                if (inSeq) {
+                    int iChain = 1;
+                    int iSeq = 2;
+                    if (line.size() > 1 && line.get(0) instanceof Integer) {
+                        String key = line.get(iChain) + "." + line.get(iSeq).toString();
+                        List<Object> values = new ArrayList<>();
+                        for (int i = 3; i < line.size(); i++) {
+                            values.add(line.get(i));
+                        }
+                        seqMap.put(key, values);
+                    } else if (line.contains("stop_")) {
+                        break;
+                    }
+                }
+            }
+        }
+        return seqMap;
+    }
+
+    private Map<String, List<Object>> buildChemShiftMap(List<List<Object>> dataArray) {
+        Map<String, List<Object>> shiftMap = new HashMap<>();
+        boolean inShift = false;
+        for (List<Object> line : dataArray) {
+            if (line.size() > 0) {
+                if (line.get(0).toString().contains("chemical_shift")) {
+                    inShift = true;
+                }
+                if (inShift) {
+                    int iChain = 0;
+                    int iSeq = 1;
+                    int iAtomName = 3;
+                    if (line.size() > 1 && line.get(1) instanceof Integer) {
+                        String key = line.get(iChain) + "." + line.get(iSeq).toString() + "." + line.get(iAtomName);
+                        List<Object> values = new ArrayList<>();
+                        for (int i = 4; i < line.size(); i++) {
+                            values.add(line.get(i));
+                        }
+                        if (shiftMap.containsKey(key)) {
+                            shiftMap.put(key + "_dup", values);
+                        } else {
+                            shiftMap.put(key, values);
+                        }
+                    } else if (line.contains("stop_")) {
+                        break;
+                    }
+                }
+            }
+        }
+        return shiftMap;
+    }
+
+    private Map<String, List<Object>> buildDistanceMap(List<List<Object>> dataArray) {
+        Map<String, List<Object>> distMap = new HashMap<>();
+        Map<Integer, String> keys = new HashMap<>();
+        boolean inDist = false;
+        for (List<Object> line : dataArray) {
+            if (line.size() > 0) {
+                if (line.get(0).toString().contains("distance_restraint")) {
+                    inDist = true;
+                }
+                if (inDist) {
+                    int iChain = 3;
+                    int iSeq = 4;
+                    int iAtomName = 6;
+                    int nAtoms = 2;
+                    if (line.size() > 1 && line.get(0) instanceof Integer) {
+                        int restraintID = (int) line.get(1);
+                        String[] keyParts = new String[nAtoms];
+                        for (int i = 0; i < keyParts.length; i++) {
+                            keyParts[i] = line.get(iChain + 4 * i) + "." + line.get(iSeq + 4 * i).toString() + "." + line.get(iAtomName + 4 * i);
+                        }
+                        String key = String.join(";", keyParts);
+                        List<Object> values = new ArrayList<>();
+                        for (int i = iAtomName + 4 * (nAtoms - 1) + 1; i < line.size(); i++) {
+                            values.add(line.get(i));
+                        }
+                        if (keys.containsKey(restraintID)) {
+                            String oldKey = keys.get(restraintID);
+                            String newKey = oldKey + "_" + key;
+                            keys.put(restraintID, newKey);
+                            distMap.remove(oldKey);
+                        } else {
+                            keys.put(restraintID, key);
+                        }
+                        key = keys.get(restraintID);
+                        distMap.put(key, values);
+                    } else if (line.contains("stop_")) {
+                        break;
+                    }
+                }
+            }
+        }
+        return distMap;
+    }
+
+    private Map<String, List<Object>> buildDihedralMap(List<List<Object>> dataArray) {
+        Map<String, List<Object>> dihedralMap = new HashMap<>();
+        boolean inDihedral = false;
+        for (List<Object> line : dataArray) {
+            if (line.size() > 0) {
+                if (line.get(0).toString().contains("dihedral_restraint")) {
+                    inDihedral = true;
+                }
+                if (inDihedral) {
+                    int iChain = 3;
+                    int iSeq = 4;
+                    int iAtomName = 6;
+                    int nAtoms = 4;
+                    if (line.size() > 1 && line.get(0) instanceof Integer) {
+                        String[] keyParts = new String[nAtoms];
+                        for (int i = 0; i < keyParts.length; i++) {
+                            keyParts[i] = line.get(iChain + 4 * i) + "." + line.get(iSeq + 4 * i).toString() + "." + line.get(iAtomName + 4 * i);
+                        }
+                        String key = String.join(";", keyParts);
+                        List<Object> values = new ArrayList<>();
+                        for (int i = iAtomName + 4 * (nAtoms - 1) + 1; i < line.size(); i++) {
+                            values.add(line.get(i));
+                        }
+                        dihedralMap.put(key, values);
+                    } else if (line.contains("stop_")) {
+                        break;
+                    }
+                }
+            }
+        }
+        return dihedralMap;
+    }
+
+    public void loadData(String nefFileName) {
+        String fileName = String.join(File.separator, "src", "test", "data","neffiles", nefFileName + ".nef");
+        String outPath = "tmp";
+        String outFile = String.join(File.separator, outPath,  nefFileName + "_nef_outTest.txt");
         try {
-            NMRNEFReader.read(fileName);
-            NMRNEFWriter.writeAll(outFile);
+            if (orig.isEmpty()) {
+                NMRNEFReader.read(fileName);
+                NMRNEFWriter.writeAll(outFile);
+                orig = convertFileLines(fileName);
+                written = convertFileLines(outFile);
+            }
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-//        File origFile = new File(fileName);
-//        File writtenFile = new File(outFile);
-        // fixme add test for file equality
     }
 
+    public boolean compareMaps(Map<String, List<Object>> origMap, Map<String, List<Object>> writtenMap) throws IOException {
+        try {
+            boolean ok = true;
+            for (Entry<String, List<Object>> entry : origMap.entrySet()) {
+                String key = entry.getKey();
+                if (!writtenMap.containsKey(key)) {
+                    System.out.println("key " + key + " not in written");
+                    ok = false;
+                } else {
+                    List<Object> origValues = entry.getValue();
+                    List<Object> writtenValues = writtenMap.get(key);
+                    for (int i = 0; i < origValues.size(); i++) {
+                        if (!origValues.get(i).equals(writtenValues.get(i))) {
+                            System.out.println(key + " " + origValues.toString() + " " + writtenValues.toString());
+                            ok = false;
+                        }
+                    }
+                }
+            }
+            for (Entry<String, List<Object>> entry : writtenMap.entrySet()) {
+                String key = entry.getKey();
+                if (!origMap.containsKey(key)) {
+                    System.out.println("key " + key + " not in orig");
+                    ok = false;
+                }
+            }
+            return ok;
+        } catch (Exception ex) {
+            return false;
+        }
+
+    }
+
+    public void testAll() throws IOException {
+        testSeqBlock();
+        testChemShiftBlock();
+        testDistanceBlock();
+    }
+
+    public void testSeqBlock() throws IOException {
+        try {
+            Map<String, List<Object>> origSeq = buildSequenceMap(orig);
+            Map<String, List<Object>> writtenSeq = buildSequenceMap(written);
+            boolean ok = compareMaps(origSeq, writtenSeq);
+            Assert.assertTrue(ok);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void testChemShiftBlock() throws IOException {
+        try {
+            Map<String, List<Object>> origShift = buildChemShiftMap(orig);
+            Map<String, List<Object>> writtenShift = buildChemShiftMap(written);
+            boolean ok = compareMaps(origShift, writtenShift);
+            Assert.assertTrue(ok);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void testDistanceBlock() throws IOException {
+        try {
+            Map<String, List<Object>> origDist = buildDistanceMap(orig);
+            Map<String, List<Object>> writtenDist = buildDistanceMap(written);
+            boolean ok = compareMaps(origDist, writtenDist);
+            Assert.assertTrue(ok);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    /*
+    @Test
+    public void testDihedralBlock() throws IOException {
+        try {
+            List<List<Object>> orig = convertFileLines(fileName);
+            List<List<Object>> written = convertFileLines(outFile);
+            Map<String, List<Object>> origDihedral = buildDihedralMap(orig);
+            Map<String, List<Object>> writtenDihedral = buildDihedralMap(written);
+            for (int i = 0; i < writtenDihedral.keySet().size(); i++) {
+                String origKey = (String) origDihedral.keySet().toArray()[i];
+                String writtenKey = (String) writtenDihedral.keySet().toArray()[i];
+                if (!origDihedral.keySet().contains(writtenKey)) {
+                    System.out.println("dihedral written: " + writtenKey + " " + writtenDihedral.get(writtenKey));
+                }
+                if (!writtenDihedral.keySet().contains(origKey)) {
+                    System.out.println("dihedral orig: " + origKey + " " + origDihedral.get(origKey));
+                }
+                if (origDihedral.keySet().equals(writtenDihedral.keySet()) && !origDihedral.get(writtenKey).equals(writtenDihedral.get(writtenKey))) {
+                    System.out.println(writtenKey + ": " + origDihedral.get(writtenKey) + " " + writtenDihedral.get(writtenKey));
+                }
+            }
+            Assert.assertTrue(writtenDihedral.equals(origDihedral));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+
+     */
 }
