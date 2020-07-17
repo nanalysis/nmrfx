de0115d07fd2337a6663985000912cae7df98cc0
Martha Beckwith <martha.beckwith@asrc.cuny.edu>
2020-07-17 16:54:59 -0400

    WIP: Add NEF residue variants to resib_iu.

--- src/main/resources/refine.py
+++ src/main/resources/refine.py
@@ -126,7 +126,7 @@ def getSequenceArray(indexing,seqString,linkers,polyType):
             if len(indices) == 1:
                startIndex = int(indices[0])
                endIndex = startIndex
-            else: 
+            else:
                 startIndex, endIndex = [int(i) for i in indices]
             resNums += range(startIndex, endIndex+1)
 
@@ -233,8 +233,8 @@ class StrictDict(dict):
 
 	# Parameters:
 
-        allowedKeys (list); list to identify allowed keys based on input specification 
-        for the type of parameters/keys to update. 
+        allowedKeys (list); list to identify allowed keys based on input specification
+        for the type of parameters/keys to update.
 
         # Returns:
 
@@ -248,12 +248,12 @@ class StrictDict(dict):
         """
         # Parameters:
 
-        changesDict (dict); a dictionary provided to update specified parameters/keys with new values. 
+        changesDict (dict); a dictionary provided to update specified parameters/keys with new values.
 
         # Returns:
 
         _ (None); If valid parameter/key in changesDict, then value will be updated. Otherwise, throws error.
-    
+
         See also: `createStrictDict(...)`
         """
         if changesDict is None:
@@ -266,12 +266,12 @@ class StrictDict(dict):
 
 class dynOptions(StrictDict):
     """
-    This subclass inherits from the StrictDict subclass. 
-    Contains a dictionary with default parameters and values used for the dynamic simulation. 
-    These default dictionary keys cannot be changed or removed. Users can only update the strict 
-    dictionary only if the keys specifed in dictionary used to update parameters exists within the 
-    list referenced by the strictDict instance variable 'allowedKeys'. Otherwise an error will be thrown. 
-    The values can be updated by instantiating a dictionary of known key parameters and values 
+    This subclass inherits from the StrictDict subclass.
+    Contains a dictionary with default parameters and values used for the dynamic simulation.
+    These default dictionary keys cannot be changed or removed. Users can only update the strict
+    dictionary only if the keys specifed in dictionary used to update parameters exists within the
+    list referenced by the strictDict instance variable 'allowedKeys'. Otherwise an error will be thrown.
+    The values can be updated by instantiating a dictionary of known key parameters and values
     which would override the default values.
 
     # Attributes:
@@ -312,8 +312,8 @@ def createStrictDict(initDict, type):
     """
     # Parameters:
 
-    * initDict (dict); a dictionary of 
-    * type (string); 
+    * initDict (dict); a dictionary of
+    * type (string);
 
     # Returns:
 
@@ -322,7 +322,7 @@ def createStrictDict(initDict, type):
     if initDict is None:
         initDict = {}
     allowedKeys = {}
-    allowedKeys['param'] = ['coarse', 'useh', 'hardSphere', 'start', 'end', 'shrinkValue', 'shrinkHValue', 'dislim', 'swap'] 
+    allowedKeys['param'] = ['coarse', 'useh', 'hardSphere', 'start', 'end', 'shrinkValue', 'shrinkHValue', 'dislim', 'swap']
     allowedKeys['force'] = ['elec', 'robson', 'repel', 'dis', 'tors', 'dih', 'irp', 'shift', 'bondWt','stack']
     allowedKeys = allowedKeys[type]
 
@@ -414,9 +414,9 @@ class refine:
         """
         # Parameters:
 
-        * ranfact (_); 
+        * ranfact (_);
         * mode (bool); Describes whether or not to use random initial angles.
-	
+
 	# Returns:
 
 	_ (None);
@@ -439,7 +439,7 @@ class refine:
         """
         # Parameters:
 
-        n (int); 
+        n (int);
 
         See also: `updateAt(...)` in Dihedrals.java
         """
@@ -486,7 +486,7 @@ class refine:
         forceWeight = ForceWeight(*forceWeights)
         self.energyLists.setForceWeight(forceWeight)
 
-    def getForces(self): 
+    def getForces(self):
         #XXX: Need to complete docstring
         """
         # Return:
@@ -600,7 +600,7 @@ class refine:
             else:
                 lower = distances - 0.001
                 upper = distances + 0.001
-      
+
             atomName1, atomName2 = bondInfo['atoms']
             self.energyLists.addDistanceConstraint(atomName1, atomName2, lower, upper, True)
 
@@ -965,7 +965,7 @@ class refine:
         if pdbFile != '':
             molio.readPDB(pdbFile)
         else:
-	    # Checks if NEF file is specified to process it. 
+	    # Checks if NEF file is specified to process it.
 	    # Even if NEF file is specified, this control flow
 	    # still checks whether 'molecule' data block is specified
             # in the YAML file.
@@ -1002,7 +1002,7 @@ class refine:
                 if 'edit' in molData:
                     self.readMolEditDict(seqReader, molData['edit'])
 
-        
+
         self.molecule = Molecule.getActive()
         self.molName = self.molecule.getName()
 
@@ -1194,8 +1194,8 @@ class refine:
 		if len(types) == 1:
 		    self.addBasePair(residue1, residue2, types[0])
 		else:
-		    self.addBasePairs(residue1, residue2, types)    
-	  	
+		    self.addBasePairs(residue1, residue2, types)
+
     def readAnnealDict(self, annealDict):
         dynDict = annealDict.get('dynOptions')
         dOpt = dynOptions(dynDict)
@@ -1294,12 +1294,13 @@ class refine:
         from org.nmrfx.processor.star import STAR3
         from org.nmrfx.structure.chemistry.io import NMRStarReader
         from org.nmrfx.structure.chemistry.io import NMRStarWriter
+        from org.nmrfx.structure.chemistry.io import NMRNEFReader
         fileReader = FileReader(fileName)
         bfR = BufferedReader(fileReader)
         star = STAR3(bfR,'star3')
         star.scanFile()
         file = File(fileName)
-        reader = NMRStarReader(file, star)
+        reader = NMRNEFReader(file, star) # NMRStarReader(file, star)
         self.dihedrals = reader.processNEF()
         self.energyLists = self.dihedrals.energyList
 
@@ -1366,7 +1367,7 @@ class refine:
 		constraints = {'atomPairs': [],'lower': lower,'upper': upper}
 		constraints['atomPairs'].append(atomPair)
 		checker[group] = constraints
-        return checker 
+        return checker
 
 #set dc [list 283.n3 698.h1 1.8 2.0]
 # 283  RCYT  H6          283  RCYT  H2'          4.00    1.00E+00
@@ -1395,11 +1396,11 @@ class refine:
                             errMsg = "Invalid number of fields: %d [file -> '%s']" % (len(fields), fIn.name)
                             errMsg += "\n\tLine : (%d) '%s'" % (lineNum+1, line)
                             raise ValueError(errMsg)
-                  
+
                         distance = float(distance)
                         # FIXME : Sometimes molecule name should be polymer name, or vice versa. ('A' instead of '2MQN')
-                        # During NEF testing, the full name of atom was prefixed using the polymer name instead of the 
-                        # molecule name. Thus, it required the quick fix below. Same had to be done in 'readCYANAAngles(..)' 
+                        # During NEF testing, the full name of atom was prefixed using the polymer name instead of the
+                        # molecule name. Thus, it required the quick fix below. Same had to be done in 'readCYANAAngles(..)'
                         molName = 'A'
                         fullAtom1 = molName+':'+res1+'.'+atom1
                         fullAtom2 = molName+':'+res2+'.'+atom2
@@ -1548,7 +1549,7 @@ class refine:
            else:
                errorString = "Number of fields in line (%d) '%s' could not be processed. [file -> '%s']" % (lineNum+1, line, fIn.name)
                fIn.close()
-               raise ValueError(errorString) 
+               raise ValueError(errorString)
            if angle in angleMap:
                atoms = angleMap[angle]
            elif (angle,resName) in angleMap:
@@ -1565,7 +1566,7 @@ class refine:
                    atom = split_atom[1]
                else:
                    dRes = 0
-	       molName = 'A' #Teddy 
+	       molName = 'A' #Teddy
                fullAtom = molName + ':' + str(res + dRes) + '.' + atom
                fullAtom = fullAtom.replace('"',"''")
                fullAtoms.append(fullAtom)
@@ -1594,7 +1595,7 @@ class refine:
 	       errMsg += "\n\t- resNum.resName: {}".format(atoms)
 	       errMsg += "\n\nHere's a list of things that could've gone wrong:\n"
 	       errMsg += "\t1) Atoms provided do not have required format to properly calculate dihedral angle(s).\n"
-	       errMsg += "\t2) Information in the constraint file does not match the information in the sequence file or the NMRFxStructure residue library." 
+	       errMsg += "\t2) Information in the constraint file does not match the information in the sequence file or the NMRFxStructure residue library."
                raise ValueError(errMsg)
 	   except:
 	       print("Internal Java Error: Need to evaluate addBoundary(...) method in Dihedral.java\n")
@@ -1723,7 +1724,7 @@ class refine:
                     atomNameI = self.getAtomName(resI,"P")
                     atomNameJ5 = self.getAtomName(resJ5,"P")
                     self.energyLists.addDistanceConstraint(atomNameI, atomNameJ5, 10, 12.0)
-                    
+
     def findHelices(self,vienna):
         polymers = self.molecule.getPolymers()
         allResidues = []
@@ -1769,9 +1770,9 @@ class refine:
 	    restraints = bp.distances[i].split(":")
 	    atoms = bp.atomPairs[i].split(":")
 	    atomI = atoms[0].split("/")[0]
-            atom1Name = self.getAtomName(residueI, atomI)						
+            atom1Name = self.getAtomName(residueI, atomI)
 	    atomJ = atoms[1].split("/")[0]
-            atom2Name = self.getAtomName(residueJ, atomJ)						
+            atom2Name = self.getAtomName(residueJ, atomJ)
 	    atomAtomDis= float(restraints[1])
 	    lowAtomAtomDis= float(restraints[0])
             atomParentDis= float(restraints[3])
@@ -1783,25 +1784,25 @@ class refine:
 	    elif atomJ.startswith("H"):
 	        parentAtom = residueJ.getAtom(atomJ).parent.getName()
                 parentAtomName = self.getAtomName(residueJ,parentAtom)
-		self.energyLists.addDistanceConstraint(parentAtomName, atom1Name ,lowAtomParentDis,atomParentDis) 
+		self.energyLists.addDistanceConstraint(parentAtomName, atom1Name ,lowAtomParentDis,atomParentDis)
 	    self.energyLists.addDistanceConstraint(atom1Name, atom2Name ,lowAtomAtomDis,atomAtomDis)
         if type == 1:
             atomPI = residueI.getAtom("P")
             atomPJ = residueJ.getAtom("P")
             if (atomPI != None) and (atomPJ != None):
-                atomPIName = self.getAtomName(residueI, "P")						
-                atomPJName = self.getAtomName(residueJ, "P")						
+                atomPIName = self.getAtomName(residueI, "P")
+                atomPJName = self.getAtomName(residueJ, "P")
                 self.energyLists.addDistanceConstraint(atomPIName, atomPJName ,14.0, 20.0)
             if addPlanarity:
                 bpRes = resNameI+resNameJ
                 if bpRes in rnaBPPlanarity:
                     planeValues = rnaBPPlanarity[bpRes]
                     for (aNameI,aNameJ,dis) in planeValues:
-                        atomIName = self.getAtomName(residueI, aNameI)						
-                        atomJName = self.getAtomName(residueJ, aNameJ)						
+                        atomIName = self.getAtomName(residueI, aNameI)
+                        atomJName = self.getAtomName(residueJ, aNameJ)
                         self.energyLists.addDistanceConstraint(atomIName, atomJName ,0.0, dis)
-        
-           
+
+
 
     def atomListGen(self, atomPair, restraints, residueI, residueJ):
 	atom1 = atomPair[0].split("/")[0]
@@ -1830,7 +1831,7 @@ class refine:
             parentAtomList2.append(parentAtomName)
 	atomList1.append(atom1Name)
 	atomList2.append(atom2Name)
-	return atomList1, atomList2, parentAtomList1, parentAtomList2, disRestraints, parentAtomDisRestraints 
+	return atomList1, atomList2, parentAtomList1, parentAtomList2, disRestraints, parentAtomDisRestraints
 
     def addBasePairs(self, residueI, residueJ, types):
         resNameI = residueI.getName()
@@ -1874,10 +1875,10 @@ class refine:
 		    parentDistances.extend(atomLists[5])
 		except:
 		    continue
-	    if len(atomList1) != 1: 
+	    if len(atomList1) != 1:
                 self.energyLists.addDistanceConstraint(atomList1, atomList2, min(distances),max(distances))
 	        self.energyLists.addDistanceConstraint(parentAtomList1, parentAtomList2, min(parentDistances),max(parentDistances))
-	    else: 
+	    else:
 		atomPair = typeAtomPairs[0][0]
 		disRestraint = restraints[0][0]
 	        atomLists = self.atomListGen(atomPair, disRestraint, residueI, residueJ)
@@ -1889,7 +1890,7 @@ class refine:
 		parentDistances.extend(atomLists[5])
 		self.energyLists.addDistanceConstraint(atomList1, atomList2, min(distances),max(distances))
                 self.energyLists.addDistanceConstraint(parentAtomList1, parentAtomList2, min(parentDistances),max(parentDistances))
-		
+
     def addStackPair(self, resI, resJ):
         resNameI = resI.getName()
         resNameJ = resJ.getName()
@@ -2065,7 +2066,7 @@ class refine:
             lowerFileName = file+'.lol'
             upperFileName = file+'.upl'
             self.readCYANADistances([lowerFileName, upperFileName],self.molName, keepSetting=self.cyanaDistanceFiles[file])
-	
+
 	for file in self.nmrfxDistanceFiles.keys():
 	    self.readNMRFxDistanceConstraints(file, keepSetting = self.nmrfxDistanceFiles[file])
 
@@ -2088,7 +2089,7 @@ class refine:
 
         # FIXME : should return the name of the file in which an error is found!
         self.addRDCConstraints()
-        
+
 
     def addDistanceConstraints(self):
         alreadyAdded = []
@@ -2121,7 +2122,7 @@ class refine:
             try:
                 self.energyLists.addDistanceConstraint(atomNames1, atomNames2, lower, upper)
             except IllegalArgumentException as IAE:
-                errMsg = "Illegal Argument received." 
+                errMsg = "Illegal Argument received."
                 errMsg += "\nJava Error Msg : %s" % (IAE.getMessage())
                 raise ValueError(errMsg)
 
@@ -2164,7 +2165,7 @@ class refine:
 
     def predictRNAShifts(self, typeRCDist="dist"):
         #XXX: Need to complete docstring
-        """Predict chemical shifts 
+        """Predict chemical shifts
 
         # Returns:
         shifts (list);
@@ -2256,9 +2257,9 @@ class refine:
     def addRingClosures(self):
         """ Close ring structure using distance constraint on specified atoms within ring."""
         ringClosures = self.molecule.getRingClosures();
-        entityIDs = [entity.getIDNum() for entity in self.molecule.getEntities()] 
-        entityNames = [entity.getName() for entity in self.molecule.getEntities()] 
-        
+        entityIDs = [entity.getIDNum() for entity in self.molecule.getEntities()]
+        entityNames = [entity.getName() for entity in self.molecule.getEntities()]
+
         if ringClosures:
             for atom1 in ringClosures:
                 atomName1 = atom1.getFullName()
@@ -2317,7 +2318,7 @@ class refine:
         stages = getAnnealStages(dOpt, self.settings)
         for stage in stages:
             runStage(stage, self, rDyn)
-				
+
         self.gmin(nsteps=dOpt['polishSteps'],tolerance=1.0e-6)
         if dOpt['dfreeSteps']> 0:
             self.refine(nsteps=dOpt['dfreeSteps'],radius=20, alg=dOpt['dfreeAlg']);
@@ -2484,7 +2485,7 @@ class refine:
         # Parameters:
 
         * fileName (string); name of angles file
-        * delta (int); 
+        * delta (int);
 
 	# Returns:
 
