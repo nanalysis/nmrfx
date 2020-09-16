dfe10a859cceb50f9c2ef248de48a60e38c9ee40
Bruce Johnson <brucejohnson@OneMoonMBPro2.local>
2020-09-16 18:05:07 -0400

    More changes for Java11

---
+++ src/main/java/org/nmrfx/processor/datasets/peaks/TreeLine.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.nmrfx.processor.datasets.peaks;
+
+/**
+ *
+ * @author brucejohnson
+ */
+public class TreeLine {
+
+    /**
+     * @return the x1
+     */
+    public double getX1() {
+        return x1;
+    }
+
+    /**
+     * @return the y1
+     */
+    public double getY1() {
+        return y1;
+    }
+
+    /**
+     * @return the x2
+     */
+    public double getX2() {
+        return x2;
+    }
+
+    /**
+     * @return the y2
+     */
+    public double getY2() {
+        return y2;
+    }
+    private final double x1;
+    private final double y1;
+    private final double x2;
+    private final double y2;
+    
+    public TreeLine(double x1, double y1, double x2, double y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+    
+}
