34e8901fa74e47396b4cf78223f52f30ce95c3bd
Bruce Johnson <bruce.johnson@asrc.cuny.edu>
2020-10-13 10:13:48 -0400

    Allow specifying ppmSet in proteinpred.predict.  Default to -1 (refSet)

--- src/main/resources/proteinpred.py
+++ src/main/resources/proteinpred.py
@@ -1,7 +1,7 @@
 from org.nmrfx.structure.chemistry.predict import ProteinPredictor
 
-def predict(mol):
+def predict(mol, ppmSet=-1):
     pred=ProteinPredictor()
     pred.init(mol)
-    pred.predict(0)
+    pred.predict(ppmSet)
 
