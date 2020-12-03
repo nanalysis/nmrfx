c5bb568cad1d8f270dcb074294976e80dc00831f
Bruce Johnson <bruce@onemoonscientific.com>
2020-12-03 13:05:28 -0500

    Add module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,24 @@
+module org.nmrfx.structure {
+    requires org.nmrfx.core;
+    requires commons.math3;
+    requires org.apache.commons.lang3;
+    requires org.apache.commons.collections4;
+    requires org.yaml.snakeyaml;
+    requires smile.data;
+    requires smile.core;
+    requires smile.interpolation;
+    requires smile.math;
+    requires io.netty.all;
+    requires java.logging;
+    requires java.desktop;
+    requires com.google.common;
+    requires janino;
+    requires commons.compiler;
+    requires jython.slim;
+    requires vecmath;
+    requires biojava.alignment;
+    requires biojava.core;
+    requires biojava.phylo;
+    requires libsvm;
+
+}
