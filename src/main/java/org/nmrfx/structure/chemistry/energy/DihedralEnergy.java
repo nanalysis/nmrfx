9c01e0c03a847bef91193d4290c2020506fa88fa
Bruce Johnson <bruce@onemoonscientific.com>
2020-12-29 13:50:30 -0500

    format

--- src/main/java/org/nmrfx/structure/chemistry/energy/DihedralEnergy.java
+++ src/main/java/org/nmrfx/structure/chemistry/energy/DihedralEnergy.java
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package org.nmrfx.structure.chemistry.energy;
 
 import org.apache.commons.math3.analysis.MultivariateFunction;
