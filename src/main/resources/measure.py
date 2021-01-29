8356324c807dbadf0816782f6f0a0d3c8479caf4
Bruce Johnson <bruce@onemoonscientific.com>
2021-01-29 14:44:10 -0500

    Add -dot flag to get vienna seq and include vienna in suite output

--- src/main/resources/measure.py
+++ src/main/resources/measure.py
@@ -4,6 +4,7 @@ import osfiles
 import argparse
 import molio
 from org.nmrfx.structure.chemistry.energy import RNARotamer
+from org.nmrfx.structure.rna import RNAAnalysis
 
 def dumpDis(mol, fileName='distances.txt', delta=0.5, atomPat='*.H*',maxDis=4.5,prob=1.1,fixLower=0.0):
     """ Writes a dump file containing distance violations based on input distance
@@ -74,20 +75,43 @@ def genResidueList(resString):
                 residues.append(sign+elem)
     return set(residues)
             
+def rnaDotBracket(mol, fileName='stdout'):
+   rnaResidues = [residue for polymer in mol.getPolymers() if polymer.isRNA() for residue in polymer.getResidues()]
+   vienna = RNAAnalysis.getViennaSequence(mol)
+   dotBracketDict = {}
+   for rnaResidue,dotBracket in zip(rnaResidues, vienna):
+       polyName = rnaResidue.getPolymer().getName()
+       resNum = rnaResidue.getNumber()
+       dotBracketDict[polyName+resNum]=str(dotBracket)
+   vienna = ''.join(vienna) 
+   return vienna,dotBracketDict
+
+def sequence(mol, fileName='stdout'):
+    for polymer in mol.getPolymers():
+        type = polymer.getPolymerType()
+        for residue in polymer.getResidues():
+            print polymer.getName(),residue.getName()
+
 def rnaSuite(mol, includeResidues, fileName='stdout'):
     if fileName == 'stdout':
         fOut = sys.stdout
     else:
         fOut = open(fileName,'w')
+    rnaDotBracketDict = None
     for polymer in mol.getPolymers():
         if polymer.isRNA():
+            if rnaDotBracketDict == None:
+                vienna, rnaDotBracketDict = rnaDotBracket(mol)
+            polyName = polymer.getName()
             for residue in polymer.getResidues():
                 if includeResidues != None:
                     if residue.getNumber() not in includeResidues:
                         continue
+                resNum = residue.getNumber()
+                dotBracket = rnaDotBracketDict[polyName+resNum]
                 rotamerScore = RNARotamer.scoreResidue(residue)
                 if rotamerScore != None:
-                    outStr = "%4s %2s %s\n" %(residue.getNumber(),residue.getName(),rotamerScore.toString())
+                    outStr = "%4s %4s %2s %2s %s\n" %(polyName, residue.getNumber(),residue.getName(),dotBracket,rotamerScore.toString())
                     fOut.write(outStr)
     if fileName != 'stdout':
         fOut.close()
@@ -114,16 +138,25 @@ def loadStructure(fileName, xMode=True, iStruct=0):
 
 def parseArgs():
     parser = argparse.ArgumentParser(description="predictor options")
+    parser.add_argument("-x", dest="xMode", default=False, action="store_true", help="Whether to read without library(False")
     parser.add_argument("-dis", dest="disMode", default=False, action="store_true", help="Whether to output  distances(False")
     parser.add_argument("-suite", dest="suiteMode", default=False, action="store_true", help="Whether to output  RNA suites(False")
+    parser.add_argument("-dot", dest="dotMode", default=False, action="store_true", help="Whether to output  RNA dot-bracket (Vienna) sequence(False")
+    parser.add_argument("-seq", dest="seqMode", default=False, action="store_true", help="Whether to output  sequence(False")
     parser.add_argument("-residues", dest="includeResidues", default='',  help="Limit residues to these (use all)")
     parser.add_argument("fileNames",nargs="*")
     args = parser.parse_args()
     includeResidues = genResidueList(args.includeResidues)
     for fileName in args.fileNames:
-        mol = loadStructure(fileName)
+        mol = loadStructure(fileName, args.xMode)
         if args.disMode:
             dumpDis(mol) 
         if args.suiteMode:
             rnaSuite(mol, includeResidues)
+        if args.dotMode:
+            vienna, rnaDotBracketDict = rnaDotBracket(mol)
+            print vienna
+        if args.seqMode:
+            sequence(mol)
+            
         
