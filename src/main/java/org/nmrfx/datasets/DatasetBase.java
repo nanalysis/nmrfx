026d370fc1dc6df5cf6b085980baa3be7c6b8158
Bruce Johnson <bruce@onemoonscientific.com>
2021-06-21 14:58:50 -0400

    Add hasLayout method Useful for checking to see if new dataset has had the layout set up yet (for dynamic resizing)

--- src/main/java/org/nmrfx/datasets/DatasetBase.java
+++ src/main/java/org/nmrfx/datasets/DatasetBase.java
@@ -2082,6 +2082,10 @@ public class DatasetBase {
         }
         return value;
     }
+    
+    public boolean hasLayout() {
+        return layout != null;
+    }
 
     synchronized public RegionData analyzeRegion(int[][] pt, int[] cpt, double[] width, int[] dim)
             throws IOException {
