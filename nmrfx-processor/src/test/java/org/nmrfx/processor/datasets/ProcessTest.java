333b247fd1843012a70f4971069344b1d3ffd387
Bruce Johnson <bruce@onemoonscientific.com>
2022-01-01 09:23:02 -0500

    Change order of arguments in assertEquals to expected,actual

--- nmrfx-processor/src/test/java/org/nmrfx/processor/datasets/ProcessTest.java
+++ nmrfx-processor/src/test/java/org/nmrfx/processor/datasets/ProcessTest.java
@@ -43,105 +43,103 @@ public class ProcessTest {
     @Test
     public void test_gb1_tract1d() throws IOException {
         long result = runAndCompare("gb1_tract1d");
-        Assert.assertEquals(result, 0);
+        Assert.assertEquals(0, result);
     }
 
     @Test
     public void test_hnconus_grins_zf() throws IOException {
         long result = runAndCompare("hnconus_grins_zf");
-        Assert.assertEquals(result, 0);
+        Assert.assertEquals(0, result);
     }
 
     @Test
     public void test_hnconus_nesta() throws IOException {
         long result = runAndCompare("hnconus_nesta");
-        Assert.assertEquals(result, 0);
+        Assert.assertEquals(0, result);
     }
 
     @Test
     public void test_jcamp_1d() throws IOException {
         long result = runAndCompare("jcamp_1d");
-        Assert.assertEquals(result, 0);
+        Assert.assertEquals(0, result);
     }
 
     @Test
     public void test_jeol_1d_1h() throws IOException {
         long result = runAndCompare("jeol_1d_1h");
-        Assert.assertEquals(result, 0);
+        Assert.assertEquals(0, result);
     }
 
     @Test
     public void test_jeol_hsqc() throws IOException {
         long result = runAndCompare("jeol_hsqc");
-        Assert.assertEquals(result, 0);
+        Assert.assertEquals(0, result);
     }
 
     @Test
     public void test_ubiq_cnoesqc() throws IOException {
         long result = runAndCompare("ubiq_cnoesqc");
-        Assert.assertEquals(result, 0);
+        Assert.assertEquals(0, result);
     }
 
     @Test
     public void test_ubiq_hnca() throws IOException {
         long result = runAndCompare("ubiq_hnca");
-        Assert.assertEquals(result, 0);
+        Assert.assertEquals(0, result);
     }
-
 
     @Test
     public void test_ubiq_hnco() throws IOException {
         long result = runAndCompare("ubiq_hnco");
-        Assert.assertEquals(result, 0);
+        Assert.assertEquals(0, result);
     }
 
     @Test
     public void test_ubiq_hnco_lp() throws IOException {
         long result = runAndCompare("ubiq_hnco_lp");
-        Assert.assertEquals(result, 0);
+        Assert.assertEquals(0, result);
     }
-
 
     @Test
     public void test_ubiq_hnco_skip2() throws IOException {
         long result = runAndCompare("ubiq_hnco_skip2");
-        Assert.assertEquals(result, 0);
+        Assert.assertEquals(0, result);
     }
 
     @Test
     public void test_ubiq_hnco_skip3() throws IOException {
         long result = runAndCompare("ubiq_hnco_skip3");
-        Assert.assertEquals(result, 0);
+        Assert.assertEquals(0, result);
     }
 
     @Test
     public void test_ubiq_hsqc() throws IOException {
         long result = runAndCompare("ubiq_hsqc");
-        Assert.assertEquals(result, 0);
+        Assert.assertEquals(0, result);
     }
 
     @Test
     public void test_ubiq_hsqc_ucsf() throws IOException {
         long result = runAndCompare("ubiq_hsqc_ucsf");
-        Assert.assertEquals(result, 0);
+        Assert.assertEquals(0, result);
     }
 
     @Test
     public void test_ubiq_noe() throws IOException {
         long result = runAndCompare("ubiq_noe");
-        Assert.assertEquals(result, 0);
+        Assert.assertEquals(0, result);
     }
 
     @Test
     public void test_ubiq_t1() throws IOException {
         long result = runAndCompare("ubiq_t1");
-        Assert.assertEquals(result, 0);
+        Assert.assertEquals(0, result);
     }
 
     @Test
     public void test_ubiq_t2() throws IOException {
         long result = runAndCompare("ubiq_t2");
-        Assert.assertEquals(result, 0);
+        Assert.assertEquals(0, result);
     }
 
 
