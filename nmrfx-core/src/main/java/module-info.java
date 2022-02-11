25453191d96a7ec6d2998c2a802ce88dab286ed4
Vincent Fiack <vfiack@rs2d.com>
2022-02-11 17:51:11 +0100

    NMR-4709: remove NMRFxServer, replaced by JMX connector

--- nmrfx-core/src/main/java/module-info.java
+++ nmrfx-core/src/main/java/module-info.java
@@ -13,7 +13,6 @@ module org.nmrfx.core {
     exports org.nmrfx.peaks.io;
     exports org.nmrfx.star;
     exports org.nmrfx.project;
-    exports org.nmrfx.server;
     exports org.nmrfx.utilities;
     requires commons.math3;
     requires org.apache.commons.lang3;
