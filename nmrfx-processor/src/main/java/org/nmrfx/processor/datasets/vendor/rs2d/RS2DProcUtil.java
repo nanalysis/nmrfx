4ed49df3e195540e9c6dddfec7f7d065a9170875
Vincent Fiack <vfiack@rs2d.com>
2022-03-23 19:30:08 +0100

    NMR-4926: add missing license & class-level javadoc

--- nmrfx-processor/src/main/java/org/nmrfx/processor/datasets/vendor/rs2d/RS2DProcUtil.java
+++ nmrfx-processor/src/main/java/org/nmrfx/processor/datasets/vendor/rs2d/RS2DProcUtil.java
@@ -1,3 +1,20 @@
+/*
+ * NMRFx: A Program for Processing NMR Data
+ * Copyright (C) 2004-2022 One Moon Scientific, Inc., Westfield, N.J., USA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.nmrfx.processor.datasets.vendor.rs2d;
 
 import org.apache.commons.lang3.StringUtils;
@@ -14,6 +31,9 @@ import java.util.OptionalInt;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+/**
+ * Utility methods to work with "Proc" folders from RS2D data.
+ */
 public class RS2DProcUtil {
     private static final Logger log = LoggerFactory.getLogger(RS2DProcUtil.class);
 
