53a120c8d3459eb6ecec7602fcff81aecb39140e
Jannalie Taylor <Jannalie.Taylor@Nanalysis.com>
2022-06-07 09:46:28 -0600

    NMR-5224: added mockito dependency and mocked LogRecord in test, renamed LogLevelCellFactory to LogLevelCellFormatter, removed unneeded super calls, set version to 'development' if null

--- nmrfx-processor-gui/src/main/java/org/nmrfx/processor/gui/log/LogLevelCellFormatter.java
+++ nmrfx-processor-gui/src/main/java/org/nmrfx/processor/gui/log/LogLevelCellFormatter.java
@@ -5,12 +5,11 @@ import javafx.scene.control.TableCell;
 import javafx.scene.paint.Color;
 import org.controlsfx.glyphfont.Glyph;
 
-import ch.qos.logback.classic.Level;
 
 /**
  * Formatter for LogLevel table cells in a LogRecord Table
  */
-public class LogLevelCellFactory extends TableCell<LogRecord, LogLevel> {
+public class LogLevelCellFormatter extends TableCell<LogRecord, LogLevel> {
     private static final String FONT_FAMILY = "FontAwesome";
 
     @Override
@@ -20,7 +19,7 @@ public class LogLevelCellFactory extends TableCell<LogRecord, LogLevel> {
             setGraphic(null);
         } else {
             // Only set the graphic, as the text is not displayed
-            setGraphic(LogLevelCellFactory.selectIcon(logLevel));
+            setGraphic(LogLevelCellFormatter.selectIcon(logLevel));
         }
     }
 
@@ -30,14 +29,14 @@ public class LogLevelCellFactory extends TableCell<LogRecord, LogLevel> {
      * @return a Glyph
      */
     public static Glyph selectIcon(LogLevel logLevel) {
-        int intValue = logLevel.getLogbackLevel().toInteger();
-        if (intValue >= Level.ERROR_INT)
+        int intValue = logLevel.ordinal();
+        if (intValue <= LogLevel.ERROR.ordinal())
             return new Glyph(FONT_FAMILY, FontAwesomeIcon.CLOSE).color(Color.RED);
-        if (intValue >= Level.WARN_INT)
+        if (intValue <= LogLevel.WARNING.ordinal())
             return new Glyph(FONT_FAMILY, FontAwesomeIcon.WARNING).color(Color.GOLD);
-        if (intValue >= Level.INFO_INT)
+        if (intValue <= LogLevel.INFO.ordinal())
             return new Glyph(FONT_FAMILY, FontAwesomeIcon.INFO_CIRCLE).color(Color.DEEPSKYBLUE);
-        if (intValue >= Level.DEBUG_INT)
+        if (intValue <= LogLevel.DEBUG.ordinal())
             return new Glyph(FONT_FAMILY, "d").color(Color.GREY);
         return new Glyph(FONT_FAMILY, FontAwesomeIcon.ELLIPSIS_H).color(Color.DARKGREY);
 
