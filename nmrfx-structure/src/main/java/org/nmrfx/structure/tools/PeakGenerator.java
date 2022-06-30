93cadd51fd56a27c9ea77db521a68fbe62379118
brucejohnson <bruce@onemoonscientific.com>
2022-06-29 22:48:04 -0400

    Update nmrfx-structure/src/main/java/org/nmrfx/structure/tools/PeakGenerator.java
    Co-authored-by: JannalieT <64663455+JannalieT@users.noreply.github.com>

--- nmrfx-structure/src/main/java/org/nmrfx/structure/tools/PeakGenerator.java
+++ nmrfx-structure/src/main/java/org/nmrfx/structure/tools/PeakGenerator.java
@@ -198,10 +198,10 @@ public class PeakGenerator {
             generateCouplings();
         }
         Atom[] atoms = new Atom[2];
-        molecule.getAtoms().stream().
-                filter(atom -> atom.getAtomicNumber() == 1).
-                filter(atom -> !atom.isMethyl() || atom.isFirstInMethyl()).
-                forEach(atom -> {
+        molecule.getAtoms().stream()
+                .filter(atom -> atom.getAtomicNumber() == 1)
+                .filter(atom -> !atom.isMethyl() || atom.isFirstInMethyl())
+                .forEach(atom -> {
                     var tocsyCouplings = tocsyCouplingMap.get(atom);
                     if ((tocsyCouplings != null)) {
                         for (var tocsyCoupling : tocsyCouplings) {
