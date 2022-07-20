c24b8ea973b4e333f5c52f86aeb2e17a350b61ff
Jannalie Taylor <Jannalie.Taylor@Nanalysis.com>
2022-07-20 08:19:42 -0600

    NMR-5313; use data.dat for RS2D FID and processed data files in extension suggestion

--- nmrfx-processor-gui/src/main/java/org/nmrfx/processor/gui/utils/FileExtensionFilterType.java
+++ nmrfx-processor-gui/src/main/java/org/nmrfx/processor/gui/utils/FileExtensionFilterType.java
@@ -10,7 +10,7 @@ import java.util.List;
  */
 public enum FileExtensionFilterType {
     ALL_FILES("All Files", "*.*"),
-    NMR_FID("NMR Fid","fid", "ser", "*.nv", "*.dx", "*.jdx", "*.jdf", "*.dat"),
+    NMR_FID("NMR Fid","fid", "ser", "*.nv", "*.dx", "*.jdx", "*.jdf", RS2DData.DATA_FILE_NAME),
     NMR_DATASET("NMR Dataset", "*.nv", "*.ucsf", "*.dx", "*.jdx", "1r", "2rr", "3rrr", "4rrrr", RS2DData.DATA_FILE_NAME),
     PDF("PDF", "*.pdf"),
     PNG("PNG", "*.png"),
