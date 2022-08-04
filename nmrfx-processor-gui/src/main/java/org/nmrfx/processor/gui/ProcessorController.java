03aa5ce16399e7c2f747b8bc7ce4336d88732c61
Jannalie Taylor <Jannalie.Taylor@Nanalysis.com>
2022-08-04 16:46:46 -0600

    NMR-5448: on canceling the processing, close the dataset

--- nmrfx-processor-gui/src/main/java/org/nmrfx/processor/gui/ProcessorController.java
+++ nmrfx-processor-gui/src/main/java/org/nmrfx/processor/gui/ProcessorController.java
@@ -963,6 +963,7 @@ public class ProcessorController implements Initializable, ProgressUpdater {
             ((Service<Integer>) worker).setOnCancelled(event -> {
                 setProcessingOff();
                 setProcessingStatus("cancelled", false);
+                Processor.getProcessor().closeDataset(false);
             });
             ((Service<Integer>) worker).setOnFailed(event -> {
                 setProcessingOff();
