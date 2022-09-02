a905254128413d700c2a3ad73de1f2a4298beff6
Jannalie Taylor <Jannalie.Taylor@Nanalysis.com>
2022-09-02 13:39:20 -0600

    NMR-5484: update dimension choice for all dimensions

--- nmrfx-processor-gui/src/main/java/org/nmrfx/processor/gui/ProcessorController.java
+++ nmrfx-processor-gui/src/main/java/org/nmrfx/processor/gui/ProcessorController.java
@@ -300,34 +300,33 @@ public class ProcessorController implements Initializable, ProgressUpdater {
 
     protected void updateDimChoice(boolean[] complex) {
         int nDim = complex.length;
-        if (nDim > 1) {
-            dimChoice.getSelectionModel().selectedItemProperty().removeListener(dimListener);
-            ObservableList<String> dimList = FXCollections.observableArrayList();
-            for (int i = 1; i <= nDim; i++) {
-                dimList.add("D" + String.valueOf(i));
-                if ((i == 1) && (nDim > 2)) {
-                    StringBuilder sBuilder = new StringBuilder();
-                    sBuilder.append("D2");
-                    for (int j = 3; j <= nDim; j++) {
-                        sBuilder.append(",");
-                        sBuilder.append(j);
-                    }
-                    dimList.add(sBuilder.toString());
+        dimChoice.getSelectionModel().selectedItemProperty().removeListener(dimListener);
+        ObservableList<String> dimList = FXCollections.observableArrayList();
+        for (int i = 1; i <= nDim; i++) {
+            dimList.add("D" + String.valueOf(i));
+            if ((i == 1) && (nDim > 2)) {
+                StringBuilder sBuilder = new StringBuilder();
+                sBuilder.append("D2");
+                for (int j = 3; j <= nDim; j++) {
+                    sBuilder.append(",");
+                    sBuilder.append(j);
                 }
+                dimList.add(sBuilder.toString());
             }
-            dimList.add("D_ALL");
-            for (int i = 1; i <= nDim; i++) {
-                dimList.add("P" + String.valueOf(i));
-                if ((i == 1) && (nDim > 2)) {
-                    dimList.add("P2,3");
-                }
-            }
-            dimChoice.setItems(dimList);
-            dimChoice.getSelectionModel().select(0);
-            dimChoice.getSelectionModel().selectedItemProperty().addListener(dimListener);
-
-            updateVecNumChoice(complex);
         }
+        dimList.add("D_ALL");
+        for (int i = 1; i <= nDim; i++) {
+            dimList.add("P" + String.valueOf(i));
+            if ((i == 1) && (nDim > 2)) {
+                dimList.add("P2,3");
+            }
+        }
+        dimChoice.setItems(dimList);
+        dimChoice.getSelectionModel().select(0);
+        dimChoice.getSelectionModel().selectedItemProperty().addListener(dimListener);
+
+        updateVecNumChoice(complex);
+
         updateLineshapeCatalog(nDim);
     }
 
