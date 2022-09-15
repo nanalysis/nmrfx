007773f9a40ff67f3f96d7b916d102f88e654322
Bruce Johnson <bruce@onemoonscientific.com>
2022-09-15 09:15:22 -0400

    Add detailed dataset compare Checks file size, header bytes and data bytes

--- nmrfx-processor/src/test/java/org/nmrfx/processor/datasets/DatasetCompare.java
+++ nmrfx-processor/src/test/java/org/nmrfx/processor/datasets/DatasetCompare.java
@@ -1,9 +1,8 @@
 package org.nmrfx.processor.datasets;
 
-import java.io.DataInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.file.Files;
 
 public class DatasetCompare {
@@ -17,6 +16,59 @@ public class DatasetCompare {
             result = Math.min(refLen, testLen);
         } else {
             result = Files.mismatch(refFile.toPath(), testFile.toPath());
+        }
+        return result;
+    }
+
+     static long[] compareParts(File refFile, File testFile) throws IOException {
+        long refLen = refFile.length();
+        long testLen = testFile.length();
+        long fileSizeError = -1;
+        long headerErrorPosition = -1;
+        long dataErrorPosition = -1;
+
+        if (refLen != testLen) {
+            fileSizeError = Math.min(refLen, testLen);
+        } else {
+            RandomAccessFile refRAFile = new RandomAccessFile(refFile, "r");
+            RandomAccessFile testRAFile = new RandomAccessFile(testFile, "r");
+
+            MappedByteBuffer refBuffer = refRAFile.getChannel().map(FileChannel.MapMode.READ_ONLY, 0, refLen);
+            MappedByteBuffer testBuffer = testRAFile.getChannel().map(FileChannel.MapMode.READ_ONLY, 0, testLen);
+            if (refBuffer.compareTo(testBuffer) != 0) {
+                int headerSize = refFile.getName().equals("data.dat") ? 0 :
+                        refFile.getName().endsWith(".nv") ? Dataset.NV_HEADER_SIZE : Dataset.UCSF_HEADER_SIZE;
+                refBuffer.position(0);
+                testBuffer.position(0);
+                for (int i = 0; i < headerSize; i++) {
+                    if (refBuffer.get(i) != testBuffer.get(i)) {
+                        headerErrorPosition = i;
+                        break;
+                    }
+                }
+                for (int i = headerSize; i < refLen; i++) {
+                    if (refBuffer.get(i) != testBuffer.get(i)) {
+                        dataErrorPosition = i;
+                        break;
+                    }
+                }
+            }
+        }
+        return new long[]{fileSizeError, dataErrorPosition, headerErrorPosition};
+    }
+
+    public static long[] compareDetailed(File refFile, File testFile) throws IOException {
+        long refLen = refFile.length();
+        long testLen = testFile.length();
+        long[] result = {-1, -1, -1};
+
+        if (refLen != testLen) {
+            result[0] = Math.min(refLen, testLen);
+        } else {
+            long matchScore = Files.mismatch(refFile.toPath(), testFile.toPath());
+            if (matchScore != -1) {
+                result = compareParts(refFile, testFile);
+            }
         }
         return result;
     }
