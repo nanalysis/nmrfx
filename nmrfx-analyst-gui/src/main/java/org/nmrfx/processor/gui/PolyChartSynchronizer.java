864db7b98367b7453d251e4b8318c441e9e7e62b
Vincent Fiack <vfiack@rs2d.com>
2023-05-30 17:16:46 +0200

    NMR-6922: use weakrefs instead of chart name to store them in groups

--- nmrfx-analyst-gui/src/main/java/org/nmrfx/processor/gui/PolyChartSynchronizer.java
+++ nmrfx-analyst-gui/src/main/java/org/nmrfx/processor/gui/PolyChartSynchronizer.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -15,34 +16,55 @@ import java.util.Objects;
 public class PolyChartSynchronizer {
     private static final Logger log = LoggerFactory.getLogger(PolyChartSynchronizer.class);
 
-    record SyncGroup(List<String> chartNames, String dimensionName) {
+    // use weak references to keep PolyChart instance free for the garbage collector
+    record SyncGroup(List<WeakReference<PolyChart>> refs, String dimensionName) {
+        public boolean contains(PolyChart chart) {
+            return refs.stream().anyMatch(ref -> ref.get() == chart);
+        }
     }
 
+    // not using a Map with dimension name as a key, because the same dimension could be used for different independent groups.
     private final List<SyncGroup> groups = new ArrayList<>();
 
     //FIXME groups are added but never removed
     //we should add a way to remove obsolete groups or stop synchronizing them
+
+    /**
+     * Synchronize charts that share the same canvas as the entry point.
+     *
+     * @param chart the reference chart to use to find related charts to synchronize.
+     */
     public void syncSceneMates(PolyChart chart) {
-        for (String name : chart.getDimNames()) {
-            addToSyncGroup(chart, name);
+        for (String dimension : chart.getDimNames()) {
+            addToSyncGroup(chart, dimension);
         }
     }
 
+    /**
+     * Synchronize axes for all charts in groups related to the reference chart. Called when the user moves a chart axis.
+     *
+     * @param chart     the chart whose axis have been moved
+     * @param axisIndex the index of the moved axis
+     * @param endNum    which axis bound has been moved
+     * @param newBound  the new upper or lower bound for this axis
+     */
     //TODO replace endNum with an enum
-    public void syncAxes(PolyChart chart, int axNum, int endNum, double newBound) {
+    public void syncAxes(PolyChart chart, int axisIndex, int endNum, double newBound) {
         if (groups.isEmpty()) {
             return;
         }
 
-        String dimensionName = chart.getDimNames().get(axNum);
+        String dimensionName = chart.getDimNames().get(axisIndex);
         SyncGroup group = findGroupForChartAndDimension(chart, dimensionName);
         if (group == null) {
             return;
         }
 
-        PolyChartManager.getInstance().getAllCharts().stream()
-                .filter(candidate -> candidate != chart)
-                .filter(candidate -> group.chartNames().contains(candidate.getName()))
+        group.refs().stream()
+                .map(WeakReference::get)
+                .filter(candidate -> candidate != chart) // don't apply changes to self
+                .filter(Objects::nonNull) // exclude references to garbage-collected charts
+                .filter(PolyChartManager.getInstance().getAllCharts()::contains) // excludes unregistered charts (even if not yet garbaged)
                 .forEach(toSync -> {
                     var axis = findAxis(toSync, dimensionName);
                     if (axis != null) {
@@ -56,29 +78,38 @@ public class PolyChartSynchronizer {
                 });
     }
 
+    /**
+     * Add the chart to an existing group, or create a new group with all its scene mates.
+     *
+     * @param chart         the reference chart for a group
+     * @param dimensionName the dimension for which to create a new synchronizationg group
+     */
     private void addToSyncGroup(PolyChart chart, String dimensionName) {
-        List<PolyChart> sceneCharts = getSceneMates(chart);
-        SyncGroup group = findGroupForChartsAndDimension(sceneCharts, dimensionName);
-        if (group == null) {
-            List<String> chartNames = sceneCharts.stream().map(PolyChart::getName).toList();
-            groups.add(new SyncGroup(new ArrayList<>(chartNames), dimensionName));
-        } else {
-            group.chartNames().add(chart.getName());
-        }
-    }
+        List<PolyChart> relatedCharts = findChartsWithSameCanvas(chart);
+        SyncGroup group = findGroupForChartsAndDimension(relatedCharts, dimensionName);
 
+        // there was already a group for these chart and dimension, remove it - it may not contain all charts
+        if (group != null) {
+            groups.remove(group);
+        }
+
+        // create a new group including the current chart
+        List<WeakReference<PolyChart>> refs = relatedCharts.stream()
+                .map(WeakReference::new)
+                .toList();
+        groups.add(new SyncGroup(refs, dimensionName));
+    }
 
     @Nullable
     private SyncGroup findGroupForChartAndDimension(PolyChart chart, String dimensionName) {
         return groups.stream()
-                .filter(g -> g.dimensionName().equals(dimensionName) && g.chartNames().contains(chart.getName()))
+                .filter(g -> g.dimensionName().equals(dimensionName) && g.contains(chart))
                 .findFirst()
                 .orElse(null);
     }
 
     @Nullable
     private SyncGroup findGroupForChartsAndDimension(List<PolyChart> charts, String dimensionName) {
-        //TODO error if more than one group found
         return charts.stream().map(chart -> findGroupForChartAndDimension(chart, dimensionName))
                 .filter(Objects::nonNull)
                 .findFirst()
@@ -90,7 +121,7 @@ public class PolyChartSynchronizer {
      *
      * @return a list of charts sharing the same canvas.
      */
-    private List<PolyChart> getSceneMates(PolyChart chart) {
+    private List<PolyChart> findChartsWithSameCanvas(PolyChart chart) {
         return PolyChartManager.getInstance().getAllCharts().stream()
                 .filter(potential -> potential.getCanvas() == chart.getCanvas())
                 .toList();
