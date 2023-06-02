bd093e5bdbc716c92f5f6dc11ad2846a104378d5
Vincent Fiack <vfiack@rs2d.com>
2023-06-02 18:51:00 +0200

    NMR-6953: field order & comments

--- nmrfx-analyst-gui/src/main/java/org/nmrfx/processor/gui/ChartDrawingLayers.java
+++ nmrfx-analyst-gui/src/main/java/org/nmrfx/processor/gui/ChartDrawingLayers.java
@@ -1,3 +1,20 @@
+/*
+ * NMRFx Processor : A Program for Processing NMR Data
+ * Copyright (C) 2004-2023 One Moon Scientific, Inc., Westfield, N.J., USA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.nmrfx.processor.gui;
 
 import javafx.scene.canvas.Canvas;
@@ -5,15 +22,26 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import org.nmrfx.processor.gui.controls.GridPaneCanvas;
 
+/**
+ * Layers on which NMR charts are drawn.
+ * These layers can be shared across multiple charts, which will have to draw at the correct coordinated by themselves using the layout information.
+ */
 public class ChartDrawingLayers {
-    // passed to chart: plotcontent, canvas, peakCanvas, annoCanvals
-    // added to chartPane: canvas, chartGroup, peakCancas, annoCanvas, plotContent
-    // difference: chartGroup, which is the gridpane
-
+    // Manages grid layout when displaying multiple charts on the same drawing canvas
+    // Also contains chart instances (as javafx Region objects)
     private final GridPaneCanvas chartGroup;
+
+    // Base layer (background): contains the spectra, axes, ...
     private final Canvas canvas = new Canvas();
+
+    // Second layer: contains peaks and annotations
     private final Canvas peakCanvas = new Canvas();
+
+    // Third layer: contains slices and drag-boxes
     private final Canvas annoCanvas = new Canvas();
+
+    // Top level (foreground): contains crosshairs, highlighted regions, selected canvas handles
+    // Equivalent to a glasspane in swing
     private final Pane plotContent = new Pane();
 
 
