b2bced2e672db4d853291a7b3358bad4d32c20d1
Vincent Fiack <vfiack@rs2d.com>
2023-06-09 15:16:24 +0200

    NMR-6871: review Yaml.load() calls, tag deserialized objects to prevent accidental changes

---
+++ nmrfx-core/src/main/java/org/nmrfx/annotations/YamlEntity.java
@@ -0,0 +1,36 @@
+/*
+ * NMRFx: A Program for Processing NMR Data
+ * Copyright (C) 2004-2023 One Moon Scientific, Inc., Westfield, N.J., USA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.nmrfx.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Used to explicitly classes loaded from Yaml files
+ * This is used to avoid changing or removing one by mistake.
+ */
+@Retention(RetentionPolicy.SOURCE)
+@Target({ElementType.TYPE, ElementType.METHOD})
+public @interface YamlEntity {
+    /**
+     * @return Known plugins using this element.
+     */
+    String[] value() default {};
+}
