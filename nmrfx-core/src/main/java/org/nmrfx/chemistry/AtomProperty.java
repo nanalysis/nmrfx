6d88e592f83f3a5d22bdd032289790bc6dc2f883
Vincent Fiack <vfiack@rs2d.com>
2023-06-12 16:52:58 +0200

    NMR-7042: enum name was always matching the atom name

--- nmrfx-core/src/main/java/org/nmrfx/chemistry/AtomProperty.java
+++ nmrfx-core/src/main/java/org/nmrfx/chemistry/AtomProperty.java
@@ -27,123 +27,121 @@ import java.util.List;
  * All atoms from the periodic table, declared specific order so that each enum value's ordinal matches with the periodic table's element number.
  */
 public enum AtomProperty {
-    X("X", 0.8f, 0.0f),
-    H("H", 1.2f, "light green", 0.0f),
-    He("He", 1.4f, "pink", 0.0f),
-    Li("Li", 1.82f, "firebrick", 0.0f),
-    B("B", 1.0f, "green", 0.0f),
-    Be("Be", 1.0f, "white", 0.0f),
-    C("C", 1.7f, "dark gray", 0.0f),
-    N("N", 1.55f, "light blue", 0.0f),
-    O("O", 1.52f, "red", 0.0f),
-    F("F", 1.47f, "goldenrod", 0.0f),
-    Ne("Ne", 1.54f, "white", 0.0f),
-    Na("Na", 2.27f, "blue", 0.0f),
-    Mg("Mg", 1.73f, "forest green", 0.0f),
-    Al("Al", 1.0f, "dark gray", 0.0f),
-    Si("Si", 2.1f, "goldenrod", 0.0f),
-    P("P", 1.8f, "orange", 0.0f),
-    S("S", 1.8f, 1.0f, 0.8f, 0.2f, 0.0f),
-    Cl("Cl", 1.75f, "green", 0.0f),
-    Ar("Ar", 1.88f, 0.0f),
-    K("K", 2.75f, 0.0f),
-    Ca("Ca", 1.0f, "dark gray", 0.0f),
-    Sc("Sc", 1.0f, 0.0f),
-    Ti("Ti", 1.0f, "dark gray", 0.0f),
-    V("V", 1.0f, 0.0f),
-    Cr("Cr", 1.0f, "dark gray", 0.0f),
-    Mn("Mn", 1.0f, "dark gray", 0.0f),
-    Fe("Fe", 1.0f, "orange", 0.0f),
-    Co("Co", 1.0f, 0.0f),
-    Ni("Ni", 1.0f, "brown", 0.0f),
-    Cu("Cu", 1.0f, "brown", 0.0f),
-    Zn("Zn", 1.0f, "brown", 0.0f),
-    Ga("Ga", 1.0f, 0.0f),
-    Ge("Ge", 1.0f, 0.0f),
-    As("As", 1.0f, 0.0f),
-    Se("Se", 1.0f, 0.0f),
-    Br("Br", 1.0f, "brown", 0.0f),
-    Kr("Kr", 1.0f, 0.0f),
-    Rb("Rb", 1.0f, 0.0f),
-    Sr("Sr", 1.0f, 0.0f),
-    Y("Y", 1.0f, 0.0f),
-    Zr("Zr", 1.0f, 0.0f),
-    Nb("Nb", 1.0f, 0.0f),
-    Mo("Mo", 1.0f, 0.0f),
-    Tc("Tc", 1.0f, 0.0f),
-    Ru("Ru", 1.0f, 0.0f),
-    Rh("Rh", 1.0f, 0.0f),
-    Pd("Pd", 1.0f, 0.0f),
-    Ag("Ag", 1.0f, "dark gray", 0.0f),
-    Cd("Cd", 1.0f, 0.0f),
-    In("In", 1.0f, 0.0f),
-    Sn("Sn", 1.0f, 0.0f),
-    Sb("Sb", 1.0f, 0.0f),
-    Te("Te", 1.0f, 0.0f),
-    I("I", 1.0f, "purple", 0.0f),
-    Xe("Xe", 1.0f, 0.0f),
-    Cs("Cs", 1.0f, 0.0f),
-    Ba("Ba", 1.0f, "orange", 0.0f),
-    La("La", 1.0f, 0.0f),
-    Ce("Ce", 1.0f, 0.0f),
-    Pr("Pr", 1.0f, 0.0f),
-    Nd("Nd", 1.0f, 0.0f),
-    Pm("Pm", 1.0f, 0.0f),
-    Sm("Sm", 1.0f, 0.0f),
-    Eu("Eu", 1.0f, 0.0f),
-    Gd("Gd", 1.0f, 0.0f),
-    Tb("Tb", 1.0f, 0.0f),
-    Dy("Dy", 1.0f, 0.0f),
-    Ho("Ho", 1.0f, 0.0f),
-    Er("Er", 1.0f, 0.0f),
-    Tm("Tm", 1.0f, 0.0f),
-    Yb("Yb", 1.0f, 0.0f),
-    Lu("Lu", 1.0f, 0.0f),
-    Hf("Hf", 1.0f, 0.0f),
-    Ta("Ta", 1.0f, 0.0f),
-    W("W", 1.0f, 0.0f),
-    Re("Re", 1.0f, 0.0f),
-    Os("Os", 1.0f, 0.0f),
-    Ir("Ir", 1.0f, 0.0f),
-    Pt("Pt", 1.0f, 0.0f),
-    Au("Au", 1.0f, "goldenrod", 0.0f),
-    Hg("Hg", 1.0f, 0.0f),
-    Tl("Tl", 1.0f, 0.0f),
-    Pb("Pb", 1.0f, 0.0f),
-    Bi("Bi", 1.0f, 0.0f),
-    Po("Po", 1.0f, 0.0f),
-    At("At", 1.0f, 0.0f),
-    Rn("Rn", 1.0f, 0.0f),
-    Fr("Fr", 1.0f, 0.0f),
-    Ra("Ra", 1.0f, 0.0f),
-    Ac("Ac", 1.0f, 0.0f),
-    Th("Th", 1.0f, 0.0f),
-    Pa("Pa", 1.0f, 0.0f),
-    U("U", 1.0f, 0.0f),
-    Np("Np", 1.0f, 0.0f),
-    Pu("Pu", 1.0f, 0.0f),
-    Am("Am", 1.0f, 0.0f),
-    Cm("Cm", 1.0f, 0.0f),
-    Bk("Bk", 1.0f, 0.0f),
-    Cf("Cf", 1.0f, 0.0f),
-    Es("Es", 1.0f, 0.0f),
-    Fm("Fm", 1.0f, 0.0f),
-    Md("Md", 1.0f, 0.0f),
-    No("No", 1.0f, 0.0f),
-    Lr("Lr", 1.0f, 0.0f);
+    X(0.8f, 0.0f),
+    H(1.2f, "light green", 0.0f),
+    He(1.4f, "pink", 0.0f),
+    Li(1.82f, "firebrick", 0.0f),
+    B(1.0f, "green", 0.0f),
+    Be(1.0f, "white", 0.0f),
+    C(1.7f, "dark gray", 0.0f),
+    N(1.55f, "light blue", 0.0f),
+    O(1.52f, "red", 0.0f),
+    F(1.47f, "goldenrod", 0.0f),
+    Ne(1.54f, "white", 0.0f),
+    Na(2.27f, "blue", 0.0f),
+    Mg(1.73f, "forest green", 0.0f),
+    Al(1.0f, "dark gray", 0.0f),
+    Si(2.1f, "goldenrod", 0.0f),
+    P(1.8f, "orange", 0.0f),
+    S(1.8f, 1.0f, 0.8f, 0.2f, 0.0f),
+    Cl(1.75f, "green", 0.0f),
+    Ar(1.88f, 0.0f),
+    K(2.75f, 0.0f),
+    Ca(1.0f, "dark gray", 0.0f),
+    Sc(1.0f, 0.0f),
+    Ti(1.0f, "dark gray", 0.0f),
+    V(1.0f, 0.0f),
+    Cr(1.0f, "dark gray", 0.0f),
+    Mn(1.0f, "dark gray", 0.0f),
+    Fe(1.0f, "orange", 0.0f),
+    Co(1.0f, 0.0f),
+    Ni(1.0f, "brown", 0.0f),
+    Cu(1.0f, "brown", 0.0f),
+    Zn(1.0f, "brown", 0.0f),
+    Ga(1.0f, 0.0f),
+    Ge(1.0f, 0.0f),
+    As(1.0f, 0.0f),
+    Se(1.0f, 0.0f),
+    Br(1.0f, "brown", 0.0f),
+    Kr(1.0f, 0.0f),
+    Rb(1.0f, 0.0f),
+    Sr(1.0f, 0.0f),
+    Y(1.0f, 0.0f),
+    Zr(1.0f, 0.0f),
+    Nb(1.0f, 0.0f),
+    Mo(1.0f, 0.0f),
+    Tc(1.0f, 0.0f),
+    Ru(1.0f, 0.0f),
+    Rh(1.0f, 0.0f),
+    Pd(1.0f, 0.0f),
+    Ag(1.0f, "dark gray", 0.0f),
+    Cd(1.0f, 0.0f),
+    In(1.0f, 0.0f),
+    Sn(1.0f, 0.0f),
+    Sb(1.0f, 0.0f),
+    Te(1.0f, 0.0f),
+    I(1.0f, "purple", 0.0f),
+    Xe(1.0f, 0.0f),
+    Cs(1.0f, 0.0f),
+    Ba(1.0f, "orange", 0.0f),
+    La(1.0f, 0.0f),
+    Ce(1.0f, 0.0f),
+    Pr(1.0f, 0.0f),
+    Nd(1.0f, 0.0f),
+    Pm(1.0f, 0.0f),
+    Sm(1.0f, 0.0f),
+    Eu(1.0f, 0.0f),
+    Gd(1.0f, 0.0f),
+    Tb(1.0f, 0.0f),
+    Dy(1.0f, 0.0f),
+    Ho(1.0f, 0.0f),
+    Er(1.0f, 0.0f),
+    Tm(1.0f, 0.0f),
+    Yb(1.0f, 0.0f),
+    Lu(1.0f, 0.0f),
+    Hf(1.0f, 0.0f),
+    Ta(1.0f, 0.0f),
+    W(1.0f, 0.0f),
+    Re(1.0f, 0.0f),
+    Os(1.0f, 0.0f),
+    Ir(1.0f, 0.0f),
+    Pt(1.0f, 0.0f),
+    Au(1.0f, "goldenrod", 0.0f),
+    Hg(1.0f, 0.0f),
+    Tl(1.0f, 0.0f),
+    Pb(1.0f, 0.0f),
+    Bi(1.0f, 0.0f),
+    Po(1.0f, 0.0f),
+    At(1.0f, 0.0f),
+    Rn(1.0f, 0.0f),
+    Fr(1.0f, 0.0f),
+    Ra(1.0f, 0.0f),
+    Ac(1.0f, 0.0f),
+    Th(1.0f, 0.0f),
+    Pa(1.0f, 0.0f),
+    U(1.0f, 0.0f),
+    Np(1.0f, 0.0f),
+    Pu(1.0f, 0.0f),
+    Am(1.0f, 0.0f),
+    Cm(1.0f, 0.0f),
+    Bk(1.0f, 0.0f),
+    Cf(1.0f, 0.0f),
+    Es(1.0f, 0.0f),
+    Fm(1.0f, 0.0f),
+    Md(1.0f, 0.0f),
+    No(1.0f, 0.0f),
+    Lr(1.0f, 0.0f);
 
     private static final String GENERIC_COLOR = "gray";
     private static final List<AtomProperty> VALUES = List.of(values());
 
-    private final String name;
     private final float radius;
     private final float red;
     private final float green;
     private final float blue;
     private final float mass;
 
-    AtomProperty(String name, float radius, float red, float green, float blue, float mass) {
-        this.name = name;
+    AtomProperty(float radius, float red, float green, float blue, float mass) {
         this.radius = radius;
         this.red = red;
         this.green = green;
@@ -151,8 +149,7 @@ public enum AtomProperty {
         this.mass = mass;
     }
 
-    AtomProperty(String name, float radius, String colorName, float mass) {
-        this.name = name;
+    AtomProperty(float radius, String colorName, float mass) {
         this.radius = radius;
 
         NMRFxColor color = NvUtil.color(colorName);
@@ -163,13 +160,8 @@ public enum AtomProperty {
     }
 
 
-    AtomProperty(String name, float radius, float mass) {
-        this(name, radius, GENERIC_COLOR, mass);
-    }
-
-    @Override
-    public String toString() {
-        return name;
+    AtomProperty(float radius, float mass) {
+        this(radius, GENERIC_COLOR, mass);
     }
 
     public float getRed() {
@@ -197,7 +189,7 @@ public enum AtomProperty {
             return null;
         }
 
-        return VALUES.get(eNum).name;
+        return VALUES.get(eNum).name();
     }
 
     public byte getElementNumber() {
