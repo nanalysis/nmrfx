4f563eb7c43f9e56653c1d784a99049d63590d69
Bruce Johnson <bruce@onemoonscientific.com>
2023-10-20 10:28:08 -0400

    Add methods to check if par exists in PeakFitPars

--- nmrfx-analyst/src/main/java/org/nmrfx/processor/optimization/PeakFitPars.java
+++ nmrfx-analyst/src/main/java/org/nmrfx/processor/optimization/PeakFitPars.java
@@ -8,4 +8,19 @@ public record PeakFitPars(Peak peak, Map<String, FitPar> fitPars) {
     public FitPar fitPar(String name) {
         return fitPars.get(name);
     }
+
+    public boolean hasPar(String parName) {
+        return fitPars.containsKey(parName);
+    }
+
+    public boolean hasPars(String... parNames) {
+        boolean ok = true;
+        for (String parName:parNames) {
+            if (!hasPar(parName)) {
+                ok = false;
+                break;
+            }
+        }
+        return ok;
+    }
 }
