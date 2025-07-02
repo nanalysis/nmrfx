0e531b87aa97fd3d15bc40fd441d51a710b88f26
ekoag <ekoag@gc.cuny.edu>
2025-07-02 16:11:39 -0400

    calculate precision, recall, f1

--- nmrfx-structure/src/test/java/org/nmrfx/structure/chemistry/predict/SSPredictorTest.java
+++ nmrfx-structure/src/test/java/org/nmrfx/structure/chemistry/predict/SSPredictorTest.java
@@ -5,30 +5,34 @@ import org.nmrfx.structure.rna.SSPredictor;
 
 import java.io.*;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class SSPredictorTest {
 
-    @Test
-    public void predict() {
-        String modelFile = "/Users/ekoag/model_143/fine_tune_model.export";
-        SSPredictor.setModelFile(modelFile);
-        SSPredictor ssPredictor = new SSPredictor();
-        String sequence = "CCCUUAUCAAGAGAGGCGGAGGGAACAGACCCUGUGAUGCCCGGCAACCUGCGGUUUGCAAGGUGCCAAUUUCUGCGGGAAAACCGGAAGAUGAGGU";
-        ssPredictor.predict(sequence);
-        ssPredictor.bipartiteMatch(0.7, 0.05, 20);
-        int n = ssPredictor.getNExtents();
-        System.out.println(n);
-        for (int i = 0; i < n ; i++) {
-            Set<SSPredictor.BasePairProbability> bpps = ssPredictor.getExtentBasePairs(i).basePairsSet();
-            System.out.println(bpps.stream().toList());
-        }
+    double[] evaluate(Set<SSPredictor.BasePairProbability> referenceSet, Set<SSPredictor.BasePairProbability> predictedSet) {
+        Set<SSPredictor.BasePairProbability> refCopy = new HashSet<>(referenceSet);
+        Set<SSPredictor.BasePairProbability> predCopy = new HashSet<>(predictedSet);
 
+        refCopy.removeAll(predictedSet);
+        predCopy.removeAll(referenceSet);
+
+        int fn = refCopy.size();
+        int fp = predCopy.size();
+
+        int tp = predictedSet.size() - fp;
+
+        double ppv = (double) tp / (tp + fp);
+        double recall = (double) tp / (tp + fn);
+        double f1 = 0.0;
+        if (ppv > 0.0 && recall > 0.0) {
+            f1 = 2 * (ppv * recall) / (ppv + recall);
+        }
+        return new double[]{ppv, recall, f1};
     }
+
     @Test
     public void predictSS() {
         String testFile = "/Users/ekoag/ssPredictorTest/mfold_validate_basepairs.txt";
-        List<Float> scores = new ArrayList<>();
+        List<double[]> scores = new ArrayList<>();
         String modelFile = "/Users/ekoag/model_143/fine_tune_model.export";
         SSPredictor.setModelFile(modelFile);
         try (BufferedReader reader = new BufferedReader(new FileReader(testFile))) {
@@ -38,44 +42,46 @@ public class SSPredictorTest {
                 String[] items = line.split(";");
                 String seq = items[0];
                 String[] bps = items[1].split(",");
+                System.out.println(seq);
                 ssPredictor.predict(seq);
                 ssPredictor.bipartiteMatch(0.7, 0.05, 20);
 
-                System.out.println("seq " + seq);
-
-                Set<SSPredictor.BasePairProbability> basepairSet = new HashSet<>();
+                Set<SSPredictor.BasePairProbability> referenceSet = new HashSet<>();
                 for (int i = 0; i < bps.length - 2; i += 2) {
                     int bp1 = Integer.parseInt(bps[i]);
                     int bp2 = Integer.parseInt(bps[i+1]);
                     SSPredictor.BasePairProbability bpp = new SSPredictor.BasePairProbability(bp1, bp2, 0.0);
-                    basepairSet.add(bpp);
+                    referenceSet.add(bpp);
                 }
 
                 int n = ssPredictor.getNExtents();
-                if (n < 1) {
-                    System.out.println("no nExtents");
-                    System.exit(0);
-                }
+                assert n > 0;
 
-                int min = 10000;
-                int nPairs = basepairSet.size();
+                double max_f1 = 0.0;
+                double[] max_result = null;
                 for (int i = 0; i < n ; i++) {
-                    Set<SSPredictor.BasePairProbability> pbpps = ssPredictor.getExtentBasePairs(i).basePairsSet();
-                    basepairSet.removeAll(pbpps);
-                    int nIncorrect = basepairSet.size();
-                    if (nIncorrect < min) {
-                        min = nIncorrect;
+                    Set<SSPredictor.BasePairProbability> predictedSet = ssPredictor.getExtentBasePairs(i).basePairsSet();
+
+                    double[] results = evaluate(referenceSet, predictedSet);
+                    System.out.println(Arrays.toString(results));
+                    double f1 = results[2];
+                    if (f1 > max_f1) {
+                        max_result = results;
                     }
                 }
-                float score = (float) (nPairs- min) / nPairs;
-                scores.add(score);
-                System.out.println(score);
+                if (max_result != null) {
+                    scores.add(max_result);
+                }
             }
-            float sum = 0;
-            for (float score : scores) {
-                sum += score;
+            double ppv_sum = 0.0;
+            double recall_sum = 0.0;
+            double f1_sum = 0.0;
+            for (double[] result : scores) {
+                ppv_sum += result[0];
+                recall_sum += result[1];
+                f1_sum += result[2];
             }
-            System.out.println(sum/scores.size());
+            System.out.println("PPV: " + ppv_sum/scores.size() + " Recall: " + recall_sum /scores.size() + "  F1: " + f1_sum/scores.size());
 
         } catch (IOException e) {
             throw new RuntimeException(e);
