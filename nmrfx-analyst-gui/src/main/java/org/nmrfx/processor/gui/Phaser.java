6b702559e31eab138413d03eb97f185f99002503
Bruce Johnson <bruce@onemoonscientific.com>
2025-07-10 21:20:37 -0400

    Finer phasing

--- nmrfx-analyst-gui/src/main/java/org/nmrfx/processor/gui/Phaser.java
+++ nmrfx-analyst-gui/src/main/java/org/nmrfx/processor/gui/Phaser.java
@@ -367,10 +367,11 @@ public class Phaser {
 
     void handlePh1Reset(double ph1, boolean updateOp) {
         ph1 = Math.round(ph1 * 10) / 10.0;
-        double start = 90.0 * Math.round(ph1 / 90.0) - 180.0;
-        double end = start + 360.0;
+        double start = 45 * Math.round(ph1 / 45) - 90.0;
+        double end = start + 180;
         sliders[1].setMin(start);
         sliders[1].setMax(end);
+        sliders[1].setBlockIncrement(0.1);
         sliders[1].setValue(ph1);
         if (updateOp) {
             setPhaseOp();
@@ -389,8 +390,8 @@ public class Phaser {
 
     protected void setPH1Slider(double value) {
         value = Math.round(value * 10) / 10.0;
-        double start = 90.0 * Math.round(value / 90.0) - 180.0;
-        double end = start + 360.0;
+        double start = 45 * Math.round(value / 45.0) - 90.0;
+        double end = start + 180.0;
         sliders[1].setMin(start);
         sliders[1].setMax(end);
         sliders[1].setValue(value);
