151d861fd662930a26ffca1cd2312846cdc289cc
ekoag <ekoag@gc.cuny.edu>
2025-10-07 14:21:05 -0400

    add method for evaluating only

--- nmrfx-structure/src/test/java/org/nmrfx/structure/chemistry/predict/SSPredictorTest.java
+++ nmrfx-structure/src/test/java/org/nmrfx/structure/chemistry/predict/SSPredictorTest.java
@@ -15,8 +15,8 @@ public class SSPredictorTest {
         Set<SSPredictor.BasePairProbability> refCopy = new HashSet<>(referenceSet);
         Set<SSPredictor.BasePairProbability> predCopy = new HashSet<>(predictedSet);
 
-        refCopy.removeAll(predictedSet);
-        predCopy.removeAll(referenceSet);
+        refCopy.removeAll(predictedSet); // missed hits (false negatives)
+        predCopy.removeAll(referenceSet); // false hits (false positives)
 
         int fn = refCopy.size();
         int fp = predCopy.size();
@@ -32,73 +32,138 @@ public class SSPredictorTest {
         return new double[]{ppv, recall, f1};
     }
 
-    public void predictSS(String testFile, String modelFile, String outFilePath) {
+    public record result(Set<SSPredictor.BasePairProbability> bpp, double[] scores) {}
+
+    public result predict(SSPredictor ssPredictor, String seq, Set<SSPredictor.BasePairProbability>  referenceSet) {
+        ssPredictor.predict(seq);
+        ssPredictor.bipartiteMatch(0.7, 0.05, 20);
+        int n = ssPredictor.getNExtents();
+        assert n > 0;
+
+        double max_f1 = -1.0;
+        result best = null;
+        for (int i = 0; i < n ; i++) {
+            Set<SSPredictor.BasePairProbability> predictedSet = ssPredictor.getExtentBasePairs(i).basePairsSet();
+
+            double[] results = evaluate(referenceSet, predictedSet);
+            double f1 = results[2];
+            if (f1 > max_f1) {
+                best = new result(predictedSet, results);
+            }
+        }
+        return best;
+    }
+
+    public void score(List<double[]> scores, StringBuilder sb) {
+        double ppv_sum = 0.0;
+        double recall_sum = 0.0;
+        double f1_sum = 0.0;
+        for (double[] result : scores) {
+            ppv_sum += result[0];
+            recall_sum += result[1];
+            f1_sum += result[2];
+        }
+        double precision = ppv_sum / scores.size();
+        double recall = recall_sum / scores.size();
+        double f1 = f1_sum / scores.size();
+        sb.append("PPV: ").append(String.format("%.3f", precision))
+                .append(" Recall: ").append(String.format("%.3f", recall))
+                .append(" F1: ").append(String.format("%.3f", f1));
+    }
+
+    public Set<SSPredictor.BasePairProbability> makeSet(String[] bps) {
+        Set<SSPredictor.BasePairProbability> bppSet = new HashSet<>();
+        for (int i = 0; i < bps.length - 2; i += 2) {
+            int bp1 = Integer.parseInt(bps[i]);
+            int bp2 = Integer.parseInt(bps[i+1]);
+            SSPredictor.BasePairProbability bpp = new SSPredictor.BasePairProbability(bp1, bp2, 0.0);
+            bppSet.add(bpp);
+        }
+        return bppSet;
+    }
+
+    public void evaluateOnly(String predFile, String expFile, String outFile) throws IOException {
         List<double[]> scores = new ArrayList<>();
-        SSPredictor.setModelFile(modelFile);
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader reader1 = new BufferedReader(new FileReader(predFile));
+             BufferedReader reader2 = new BufferedReader(new FileReader(expFile))) {
+            String line1;
+            String line2;
+
+            while ((line1 = reader1.readLine()) != null && (line2 = reader2.readLine()) != null) {
+                String[] items = line1.split(";");
+                String seq1 = items[0];
+                String[] bps1 = {};
+                Set<SSPredictor.BasePairProbability> referenceSet = makeSet(bps1);
+                String[] items2 = line2.split(";");
+                String seq2 = items2[0];
+                assert seq1.equals(seq2);
+                String[] bps2 = {};
+                Set<SSPredictor.BasePairProbability> predSet = makeSet(bps2);
+                double[] results = evaluate(referenceSet, predSet);
+                scores.add(results);
+
+                sb.append(seq1).append(" ");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        score(scores, sb);
+        try (FileWriter fileWriter = new FileWriter(outFile)) {
+            fileWriter.write(sb.toString());
+        }
+    }
+
+    public void inference(String testFile, String modelPath, String outFile, boolean doScoring) {
+        List<double[]> scores = new ArrayList<>();
+        SSPredictor.setModelFile(modelPath);
         try (BufferedReader reader = new BufferedReader(new FileReader(testFile))) {
             String line;
             StringBuilder sb = new StringBuilder();
             while((line = reader.readLine()) != null) {
-                SSPredictor ssPredictor = new SSPredictor();
                 String[] items = line.split(";");
                 String seq = items[0];
                 String[] bps = {};
                 if (!items[1].isEmpty()) {
                     bps = items[1].split(",");
                 }
-                System.out.println(seq);
-                ssPredictor.predict(seq);
-                ssPredictor.bipartiteMatch(0.7, 0.05, 20);
+                sb.append(seq).append(";");
 
-                Set<SSPredictor.BasePairProbability> referenceSet = new HashSet<>();
-                for (int i = 0; i < bps.length - 2; i += 2) {
-                    int bp1 = Integer.parseInt(bps[i]);
-                    int bp2 = Integer.parseInt(bps[i+1]);
-                    SSPredictor.BasePairProbability bpp = new SSPredictor.BasePairProbability(bp1, bp2, 0.0);
-                    referenceSet.add(bpp);
-                }
+                SSPredictor ssPredictor = new SSPredictor();
+                Set<SSPredictor.BasePairProbability> referenceSet = makeSet(bps);
+                result best = predict(ssPredictor, seq, referenceSet);
 
-                int n = ssPredictor.getNExtents();
-                assert n > 0;
-
-                double max_f1 = -1.0;
-                double[] max_result = null;
-                for (int i = 0; i < n ; i++) {
-                    Set<SSPredictor.BasePairProbability> predictedSet = ssPredictor.getExtentBasePairs(i).basePairsSet();
-
-                    double[] results = evaluate(referenceSet, predictedSet);
-                    System.out.println(Arrays.toString(results));
-                    double f1 = results[2];
-                    if (f1 > max_f1) {
-                        max_result = results;
+                if (best != null) {
+                    if (doScoring) {
+                        scores.add(best.scores);
+                        Arrays.stream(best.scores).forEach(result -> sb.append(String.format("%.3f", result)).append(" "));
+                        sb.append("\n");
+                    } else {
+                        best.bpp.forEach(bpp -> sb.append(bpp.c()).append(",").append(bpp.r()).append(","));
+                        sb.deleteCharAt(sb.length() - 1);
+                        sb.append("\n");
                     }
                 }
-                if (max_result != null) {
-                    scores.add(max_result);
-                    Arrays.stream(max_result).forEach(result -> sb.append(result).append(" "));
-                    sb.append("\n");
-                }
             }
-            try (FileWriter fileWriter = new FileWriter(outFilePath)) {
-                fileWriter.write(sb.toString());
+            if (doScoring) {
+                score(scores, sb);
             }
 
-            double ppv_sum = 0.0;
-            double recall_sum = 0.0;
-            double f1_sum = 0.0;
-            for (double[] result : scores) {
-                ppv_sum += result[0];
-                recall_sum += result[1];
-                f1_sum += result[2];
+            try (FileWriter fileWriter = new FileWriter(outFile)) {
+                fileWriter.write(sb.toString());
             }
-            double precision = ppv_sum/scores.size();
-            double recall = recall_sum /scores.size();
-            double f1 = f1_sum/scores.size();
-            System.out.println("PPV: " + precision + " Recall: " +recall + "  F1: " + f1);
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
+    }
+@Test
+public void runTest() {
+        String testFile = "/Users/ekoag/rna_ss_models/RNAStrAlign_test.txt";
+        String modelPath = "/Users/ekoag/rna_ss_models/model_224/fine_tune_model.export/";
+        String outFile = "/Users/ekoag/rna_ss_models/predicted_bps.out";
+        inference(testFile, modelPath, outFile, false);
     }
 }
