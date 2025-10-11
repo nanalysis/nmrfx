4d4facdabddaed586c8c0d70f9248140b8bd9ab1
Bruce Johnson <bruce@onemoonscientific.com>
2025-10-11 11:54:48 -0400

    Add AtomPaths

--- nmrfx-structure/src/main/java/org/nmrfx/structure/chemistry/predict/ResidueAtomDistances.java
+++ nmrfx-structure/src/main/java/org/nmrfx/structure/chemistry/predict/ResidueAtomDistances.java
@@ -1,5 +1,7 @@
 package org.nmrfx.structure.chemistry.predict;
 
+import org.jgrapht.alg.shortestpath.DefaultManyToManyShortestPaths;
+import org.jgrapht.graph.DefaultEdge;
 import org.nmrfx.chemistry.Atom;
 import org.nmrfx.chemistry.Compound;
 import org.nmrfx.chemistry.PPMv;
@@ -10,11 +12,9 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class ResidueAtomDistances {
     List<AtomGraph> atomGraphs = new ArrayList<>();
-    Map<String, Integer> atomMap = Map.of("H",1, "C",2,"N",3, "O", 4,"P", 5);
 
     public record AtomNode(int index, int property, double ppm, int mask) {
         public String getCSV(int iGraph) {
@@ -22,32 +22,33 @@ public class ResidueAtomDistances {
         }
     }
 
-    public record AtomEdge(int indexA, int indexB, double distance, int bondDis) {
+    public record AtomEdge(int indexA, int indexB, double distance, int pathLen) {
         public String getCSV(int iGraph) {
-            return String.format("%d,%d,%d,%.3f,%d", iGraph, indexA, indexB, distance, bondDis);
+            return String.format("%d,%d,%d,%.3f,%d", iGraph, indexA, indexB, distance, pathLen);
         }
     }
 
     public record AtomGraph(List<AtomNode> nodes, List<AtomEdge> edges) {
         public String getNodesCSV(int iGraph) {
-            StringBuilder stringBuilder  = new StringBuilder();
+            StringBuilder stringBuilder = new StringBuilder();
             for (AtomNode atomNode : nodes) {
                 stringBuilder.append(atomNode.getCSV(iGraph));
                 stringBuilder.append("\n");
             }
-            return  stringBuilder.toString();
+            return stringBuilder.toString();
         }
+
         public String getEdgesCSV(int iGraph) {
-            StringBuilder stringBuilder  = new StringBuilder();
+            StringBuilder stringBuilder = new StringBuilder();
             for (AtomEdge atomEdge : edges) {
                 stringBuilder.append(atomEdge.getCSV(iGraph));
                 stringBuilder.append("\n");
             }
-            return  stringBuilder.toString();
+            return stringBuilder.toString();
         }
     }
 
-    AtomGraph getNodesAndEdges(List<Compound> compounds, int iStruct, double limit) {
+    AtomGraph getNodesAndEdges(List<Compound> compounds, DefaultManyToManyShortestPaths<Atom, DefaultEdge> paths, int iStruct, double limit) {
         Compound compound0 = compounds.getFirst();
         List<Atom> atoms = compounds.stream().flatMap(compound -> compound.atoms.stream()).toList();
         AtomGraph atomGraph = new AtomGraph(new ArrayList<>(), new ArrayList<>());
@@ -55,9 +56,9 @@ public class ResidueAtomDistances {
             Atom atomA = atoms.get(iAtomA);
             int atomicNumber = atomA.getAtomicNumber();
             if (atomicNumber < 1) {
-                System.out.println("no atom " + atomicNumber + " " + atomA.getElementName());
                 continue;
-            }            int useNode = atomA.getEntity() == compound0 ? 1 : 0;
+            }
+            int useNode = atomA.getEntity() == compound0 ? 1 : 0;
             Point3 pointA = atomA.getPoint(iStruct);
             PPMv ppmV = atomA.getPPM(iStruct);
             double ppm;
@@ -76,8 +77,9 @@ public class ResidueAtomDistances {
                     Point3 pointB = atomB.getPoint(iStruct);
                     double distance = pointA.distance(pointB);
                     if (distance < limit) {
-                        int bondDis = atomA.isBonded(atomB) ? 1 : 0;
-                        AtomEdge atomEdge = new AtomEdge(iAtomA, iAtomB, distance, bondDis);
+                        var path = paths.getPath(atomA, atomB);
+                        int pathLen = path != null ? path.getLength() : 0;
+                        AtomEdge atomEdge = new AtomEdge(iAtomA, iAtomB, distance, pathLen);
                         atomGraph.edges.add(atomEdge);
                     }
                 }
@@ -86,29 +88,32 @@ public class ResidueAtomDistances {
         return atomGraph;
     }
 
-    public void generate(Molecule molecule, double limit, int iStruct) {
+    public void generate(Molecule molecule, DefaultManyToManyShortestPaths<Atom, DefaultEdge> paths, double limit, int iStruct) {
         List<Compound> residueList = new ArrayList<>();
         residueList.addAll(molecule.getPolymers().stream()
                 .flatMap(polymer -> polymer.getResidues()
-                        .stream().map(residue -> (Compound) residue)).toList());
+                        .stream().map(Compound.class::cast)).toList());
         residueList.addAll(molecule.getLigands());
         for (Compound residueA : residueList) {
             List<Compound> compounds = new ArrayList<>();
             compounds.add(residueA);
             for (Compound residueB : residueList) {
-                if (residueA != residueB) {
-                    if (residueA.overlaps(residueB, limit, iStruct)) {
-                        compounds.add(residueB);
-                    }
+                if ((residueA != residueB) && (residueA.overlaps(residueB, limit, iStruct))) {
+                    compounds.add(residueB);
                 }
             }
-            atomGraphs.add(getNodesAndEdges(compounds, iStruct, limit));
+            atomGraphs.add(getNodesAndEdges(compounds, paths, iStruct, limit));
         }
+    }
+
+    public void generate(Compound compound, DefaultManyToManyShortestPaths<Atom, DefaultEdge> paths, double limit, int iStruct) {
+        List<Compound> compounds = List.of(compound);
+        atomGraphs.add(getNodesAndEdges(compounds, paths, iStruct, limit));
     }
 
     public void dumpGraphs(String nodeFileName, String edgeFileName) throws IOException {
         String nodeHeader = "graph_id,node_id,node_type,label,mask\n";
-        String edgeHeader = "graph_id,source,target,weight,type\n";
+        String edgeHeader = "graph_id,source,target,weight,nbonds\n";
         try (FileWriter fileWriter = new FileWriter(nodeFileName)) {
             fileWriter.write(nodeHeader);
             int i = 0;
