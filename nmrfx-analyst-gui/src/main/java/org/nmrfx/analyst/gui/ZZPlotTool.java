d7208afa658929cf499b559e8621f5b15a3a772c
Bruce Johnson <bruce@onemoonscientific.com>
2026-03-21 15:32:47 -0400

    Fix extent of simulated line

--- nmrfx-analyst-gui/src/main/java/org/nmrfx/analyst/gui/ZZPlotTool.java
+++ nmrfx-analyst-gui/src/main/java/org/nmrfx/analyst/gui/ZZPlotTool.java
@@ -746,7 +746,8 @@ public class ZZPlotTool {
                 }
             });
 
-            double xMax = xValues[xValues.length - 1];
+
+            double xMax = series.getMaxX();
             int nPoints = 100;
             for (int i = 0; i < nPoints; i++) {
                 double delay = i * xMax / (nPoints - 1);
